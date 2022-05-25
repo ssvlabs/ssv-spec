26728c3bc11406e9552f7d012e245437c8791311
Alon Muroch <alonmuroch@gmail.com>
2022-05-25 14:45:10 +0300

    proposal justifications test stubs

--- qbft/spectest/tests/proposal/prepared_previously_no_justification.go
+++
@@ -1,8 +0,0 @@
-package proposal
-
-import "github.com/bloxapp/ssv-spec/qbft/spectest/tests"
-
-// PreparedPreviouslyNoJustification tests a proposal for > 1 round, prepared previously but without justification
-func PreparedPreviouslyNoJustification() *tests.MsgProcessingSpecTest {
-	panic("implement")
-}
--- qbft/spectest/tests/proposal/previous_proposal_for_round.go
+++ qbft/spectest/tests/proposal/previous_proposal_for_round.go
@@ -2,7 +2,7 @@ package proposal
 
 import "github.com/bloxapp/ssv-spec/qbft/spectest/tests"
 
-// PreviousProposalForRound tests a second proposal for current round. state.ProposalAcceptedForCurrentRound != nil && signedProposal.Message.Round == state.Round
+// PreviousProposalForRound tests a second proposal (by same signer) for current round. state.ProposalAcceptedForCurrentRound != nil && signedProposal.Message.Round == state.Round
 func PreviousProposalForRound() *tests.MsgProcessingSpecTest {
 	panic("implement")
 }
