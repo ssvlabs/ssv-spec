2fd30e881e40214c98bd2ff7e92216aeb9d407dd
gldeng <realgldeng@gmail.com>
2022-06-24 18:12:40 +0800

    Remove duplicate KeyGenOutput

--- dkg/protocol.go
+++ dkg/protocol.go
@@ -1,18 +1,5 @@
 package dkg
 
-import (
-	"github.com/bloxapp/ssv-spec/types"
-	"github.com/herumi/bls-eth-go-binary/bls"
-)
-
-// KeyGenOutput is the bare minimum output from the protocol
-type KeyGenOutput struct {
-	Share           *bls.SecretKey
-	OperatorIDs     []types.OperatorID
-	OperatorPubKeys []bls.PublicKey
-	ValidatorPK     types.ValidatorPK
-}
-
 // Protocol is an interface for all DKG protocol to support a variety of protocols for future upgrades
 type Protocol interface {
 	Start() ([]Message, error)
