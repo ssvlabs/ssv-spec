df26285ca66136526f71ca693cfde1a6fa3e5a0d
gldeng <realgldeng@gmail.com>
2022-07-25 12:42:26 +0800

    Clean up

--- dkg/types/constants.go
+++ dkg/types/constants.go
@@ -6,8 +6,6 @@ const (
 	InitMsgType
 	// ProtocolMsgType is the DKG itself
 	ProtocolMsgType
-	KeygenOutputType
-	PartialOutputMsgType
 	// PartialSingatureMsgType post DKG deposit data signatures
 	PartialSingatureMsgType
 	// SignedDepositDataMsgType final output msg used by requester to make deposits and register validator with SSV
