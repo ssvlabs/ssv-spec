0cd91076523728af1e305bef2078ed5089468dc2
moshe-blox <moshe@blox.io>
2022-11-23 13:39:00 +0200

    queue: split spectest into files

--- ssv/spectest/tests/queue/no_running_instance.go
+++ ssv/spectest/tests/queue/no_running_instance.go
@@ -6,57 +6,6 @@ import (
 	"github.com/bloxapp/ssv-spec/ssv/queue"
 )
 
-func RunningInstance() *MessagePrioritySliceTest {
-	return &MessagePrioritySliceTest{
-		Name: "Running instance",
-		State: &queue.State{
-			HasRunningInstance: true,
-			Height:             100,
-			Slot:               64,
-			Quorum:             4,
-		},
-		Permutations: [][]int{
-			{12, 4, 2, 6, 10, 0, 3, 11, 7, 5, 8, 1, 9},
-			{8, 0, 2, 11, 1, 6, 5, 12, 9, 4, 10, 3, 7},
-			{5, 12, 1, 8, 0, 2, 9, 11, 3, 6, 4, 7, 10},
-		},
-		Messages: []mockMessage{
-			// 1. Current height/slot:
-			// 1.1. Consensus
-			// 1.1.1. Consensus/Prepare
-			{Consensus: &mockConsensusMessage{Height: 100, Type: qbft.PrepareMsgType}},
-			// 1.1.2. Consensus/Proposal
-			{Consensus: &mockConsensusMessage{Height: 100, Type: qbft.ProposalMsgType}},
-			// 1.1.3. Consensus/Commit
-			{Consensus: &mockConsensusMessage{Height: 100, Type: qbft.CommitMsgType}},
-			// 1.1.4. Consensus/<Other>
-			{Consensus: &mockConsensusMessage{Height: 100, Type: qbft.RoundChangeMsgType}},
-			// 1.2. Pre-consensus
-			{NonConsensus: &mockNonConsensusMessage{Slot: 64, Type: ssv.SelectionProofPartialSig}},
-			// 1.3. Post-consensus
-			{NonConsensus: &mockNonConsensusMessage{Slot: 64, Type: ssv.PostConsensusPartialSig}},
-
-			// 2. Higher height/slot:
-			// 2.1 Decided
-			{Consensus: &mockConsensusMessage{Height: 101, Decided: true}},
-			// 2.2. Pre-consensus
-			{NonConsensus: &mockNonConsensusMessage{Slot: 65, Type: ssv.SelectionProofPartialSig}},
-			// 2.3. Consensus
-			{Consensus: &mockConsensusMessage{Height: 101}},
-			// 2.4. Post-consensus
-			{NonConsensus: &mockNonConsensusMessage{Slot: 65, Type: ssv.PostConsensusPartialSig}},
-
-			// 3. Lower height/slot:
-			// 3.1 Decided
-			{Consensus: &mockConsensusMessage{Height: 99, Decided: true}},
-			// 3.2. Commit
-			{Consensus: &mockConsensusMessage{Height: 99, Type: qbft.CommitMsgType}},
-			// 3.3. Pre-consensus
-			{NonConsensus: &mockNonConsensusMessage{Slot: 63, Type: ssv.SelectionProofPartialSig}},
-		},
-	}
-}
-
 func NoRunningInstance() *MessagePrioritySliceTest {
 	return &MessagePrioritySliceTest{
 		Name: "No running instance",
