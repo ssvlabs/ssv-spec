1f893a4289f0f5ce31cd073f4fb3f14ad622ba3e
Alon Muroch <alonmuroch@gmail.com>
2022-12-27 10:09:02 +0200

    validator registration runner consensus and post consensus tests

---
+++ ssv/spectest/tests/runner/consensus/valid_message.go
@@ -0,0 +1,164 @@
+package consensus
+
+import (
+	"github.com/bloxapp/ssv-spec/qbft"
+	"github.com/bloxapp/ssv-spec/ssv"
+	"github.com/bloxapp/ssv-spec/ssv/spectest/tests"
+	"github.com/bloxapp/ssv-spec/types"
+	"github.com/bloxapp/ssv-spec/types/testingutils"
+)
+
+// ValidMessage tests a valid consensus message
+func ValidMessage() *tests.MultiMsgProcessingSpecTest {
+	ks := testingutils.Testing4SharesSet()
+	return &tests.MultiMsgProcessingSpecTest{
+		Name: "consensus valid message",
+		Tests: []*tests.MsgProcessingSpecTest{
+			{
+				Name:   "sync committee contribution",
+				Runner: testingutils.SyncCommitteeContributionRunner(ks),
+				Duty:   testingutils.TestingSyncCommitteeContributionDuty,
+				Messages: []*types.SSVMessage{
+					testingutils.SSVMsgSyncCommitteeContribution(nil, testingutils.PreConsensusContributionProofMsg(ks.Shares[1], ks.Shares[1], 1, 1)),
+					testingutils.SSVMsgSyncCommitteeContribution(nil, testingutils.PreConsensusContributionProofMsg(ks.Shares[2], ks.Shares[2], 2, 2)),
+					testingutils.SSVMsgSyncCommitteeContribution(nil, testingutils.PreConsensusContributionProofMsg(ks.Shares[3], ks.Shares[3], 3, 3)),
+					testingutils.SSVMsgSyncCommitteeContribution(
+						testingutils.SignQBFTMsg(testingutils.Testing4SharesSet().Shares[1], types.OperatorID(1), &qbft.Message{
+							MsgType:    qbft.ProposalMsgType,
+							Height:     qbft.FirstHeight,
+							Round:      qbft.FirstRound,
+							Identifier: testingutils.SyncCommitteeContributionMsgID,
+							Data:       testingutils.ProposalDataBytes(testingutils.TestSyncCommitteeContributionConsensusDataByts, nil, nil),
+						}), nil),
+				},
+				PostDutyRunnerStateRoot: "927c42ba49020b7420e315d64f0d8cde6bc8003f066f37b9e555c390373f9d4c",
+				OutputMessages: []*ssv.SignedPartialSignatureMessage{
+					testingutils.PreConsensusContributionProofMsg(ks.Shares[1], ks.Shares[1], 1, 1),
+				},
+			},
+			{
+				Name:   "sync committee",
+				Runner: testingutils.SyncCommitteeRunner(ks),
+				Duty:   testingutils.TestingSyncCommitteeDuty,
+				Messages: []*types.SSVMessage{
+					testingutils.SSVMsgSyncCommittee(
+						testingutils.SignQBFTMsg(testingutils.Testing4SharesSet().Shares[1], types.OperatorID(1), &qbft.Message{
+							MsgType:    qbft.ProposalMsgType,
+							Height:     qbft.FirstHeight,
+							Round:      qbft.FirstRound,
+							Identifier: testingutils.SyncCommitteeMsgID,
+							Data:       testingutils.ProposalDataBytes(testingutils.TestSyncCommitteeConsensusDataByts, nil, nil),
+						}), nil),
+				},
+				PostDutyRunnerStateRoot: "333087e5fe1f3c8a68f87157e195386b3b12f07533357972ddb0de979c501982",
+				OutputMessages:          []*ssv.SignedPartialSignatureMessage{},
+			},
+			{
+				Name:   "aggregator",
+				Runner: testingutils.AggregatorRunner(ks),
+				Duty:   testingutils.TestingAggregatorDuty,
+				Messages: []*types.SSVMessage{
+					testingutils.SSVMsgAggregator(nil, testingutils.PreConsensusSelectionProofMsg(ks.Shares[1], ks.Shares[1], 1, 1)),
+					testingutils.SSVMsgAggregator(nil, testingutils.PreConsensusSelectionProofMsg(ks.Shares[2], ks.Shares[2], 2, 2)),
+					testingutils.SSVMsgAggregator(nil, testingutils.PreConsensusSelectionProofMsg(ks.Shares[3], ks.Shares[3], 3, 3)),
+					testingutils.SSVMsgAggregator(
+						testingutils.SignQBFTMsg(testingutils.Testing4SharesSet().Shares[1], types.OperatorID(1), &qbft.Message{
+							MsgType:    qbft.ProposalMsgType,
+							Height:     qbft.FirstHeight,
+							Round:      qbft.FirstRound,
+							Identifier: testingutils.AggregatorMsgID,
+							Data:       testingutils.ProposalDataBytes(testingutils.TestAggregatorConsensusDataByts, nil, nil),
+						}), nil),
+				},
+				PostDutyRunnerStateRoot: "bf4001653e047c1993de144424b11cd7ac3f35a6f5c7c6b5b12b2a4dda7b4b40",
+				OutputMessages: []*ssv.SignedPartialSignatureMessage{
+					testingutils.PreConsensusSelectionProofMsg(ks.Shares[1], ks.Shares[1], 1, 1),
+				},
+			},
+			{
+				Name:   "proposer",
+				Runner: testingutils.ProposerRunner(ks),
+				Duty:   testingutils.TestingProposerDuty,
+				Messages: []*types.SSVMessage{
+					testingutils.SSVMsgProposer(nil, testingutils.PreConsensusRandaoMsg(ks.Shares[1], 1)),
+					testingutils.SSVMsgProposer(nil, testingutils.PreConsensusRandaoMsg(ks.Shares[2], 2)),
+					testingutils.SSVMsgProposer(nil, testingutils.PreConsensusRandaoMsg(ks.Shares[3], 3)),
+					testingutils.SSVMsgProposer(
+						testingutils.SignQBFTMsg(testingutils.Testing4SharesSet().Shares[1], types.OperatorID(1), &qbft.Message{
+							MsgType:    qbft.ProposalMsgType,
+							Height:     qbft.FirstHeight,
+							Round:      qbft.FirstRound,
+							Identifier: testingutils.ProposerMsgID,
+							Data:       testingutils.ProposalDataBytes(testingutils.TestProposerConsensusDataByts, nil, nil),
+						}), nil),
+				},
+				PostDutyRunnerStateRoot: "e3c52ea0bdd1649b171635d771e9254e165d5c9286115cf4191cdb0af1d96af9",
+				OutputMessages: []*ssv.SignedPartialSignatureMessage{
+					testingutils.PreConsensusRandaoMsg(testingutils.Testing4SharesSet().Shares[1], 1),
+				},
+			},
+			{
+				Name:   "proposer blinded block",
+				Runner: testingutils.ProposerBlindedBlockRunner(ks),
+				Duty:   testingutils.TestingProposerDuty,
+				Messages: []*types.SSVMessage{
+					testingutils.SSVMsgProposer(nil, testingutils.PreConsensusRandaoMsg(ks.Shares[1], 1)),
+					testingutils.SSVMsgProposer(nil, testingutils.PreConsensusRandaoMsg(ks.Shares[2], 2)),
+					testingutils.SSVMsgProposer(nil, testingutils.PreConsensusRandaoMsg(ks.Shares[3], 3)),
+					testingutils.SSVMsgProposer(
+						testingutils.SignQBFTMsg(testingutils.Testing4SharesSet().Shares[1], types.OperatorID(1), &qbft.Message{
+							MsgType:    qbft.ProposalMsgType,
+							Height:     qbft.FirstHeight,
+							Round:      qbft.FirstRound,
+							Identifier: testingutils.ProposerMsgID,
+							Data:       testingutils.ProposalDataBytes(testingutils.TestProposerConsensusDataByts, nil, nil),
+						}), nil),
+				},
+				PostDutyRunnerStateRoot: "e4d9a7acc0014a169e3de5ab00b4d4de36d6ad0a448b4462604e335c597e44de",
+				OutputMessages: []*ssv.SignedPartialSignatureMessage{
+					testingutils.PreConsensusRandaoMsg(testingutils.Testing4SharesSet().Shares[1], 1),
+				},
+			},
+			{
+				Name:   "attester",
+				Runner: testingutils.AttesterRunner(ks),
+				Duty:   testingutils.TestingAttesterDuty,
+				Messages: []*types.SSVMessage{
+					testingutils.SSVMsgAttester(
+						testingutils.SignQBFTMsg(testingutils.Testing4SharesSet().Shares[1], types.OperatorID(1), &qbft.Message{
+							MsgType:    qbft.ProposalMsgType,
+							Height:     qbft.FirstHeight,
+							Round:      qbft.FirstRound,
+							Identifier: testingutils.AttesterMsgID,
+							Data:       testingutils.ProposalDataBytes(testingutils.TestAttesterConsensusDataByts, nil, nil),
+						}), nil),
+				},
+				PostDutyRunnerStateRoot: "8dfac9e5e8ea14841cc3762f9dd3bf5598c3b2dcaa65f51b0a0db34771ee4b94",
+				OutputMessages:          []*ssv.SignedPartialSignatureMessage{},
+			},
+			{
+				Name:   "validator registration",
+				Runner: testingutils.ValidatorRegistrationRunner(ks),
+				Duty:   testingutils.TestingValidatorRegistrationDuty,
+				Messages: []*types.SSVMessage{
+					testingutils.SSVMsgValidatorRegistration(nil, testingutils.PreConsensusValidatorRegistrationMsg(ks.Shares[1], 1)),
+					testingutils.SSVMsgValidatorRegistration(nil, testingutils.PreConsensusValidatorRegistrationMsg(ks.Shares[2], 2)),
+					testingutils.SSVMsgValidatorRegistration(nil, testingutils.PreConsensusValidatorRegistrationMsg(ks.Shares[3], 3)),
+					testingutils.SSVMsgValidatorRegistration(
+						testingutils.SignQBFTMsg(testingutils.Testing4SharesSet().Shares[1], types.OperatorID(1), &qbft.Message{
+							MsgType:    qbft.ProposalMsgType,
+							Height:     qbft.FirstHeight,
+							Round:      qbft.FirstRound,
+							Identifier: testingutils.ValidatorRegistrationMsgID,
+							Data:       testingutils.ProposalDataBytes(testingutils.TestAttesterConsensusDataByts, nil, nil),
+						}), nil),
+				},
+				PostDutyRunnerStateRoot: "ff6b65005d3abe4844d7a11b947d4e920da075ee47bac92e45584ca7ed875fdb",
+				OutputMessages: []*ssv.SignedPartialSignatureMessage{
+					testingutils.PreConsensusValidatorRegistrationMsg(ks.Shares[1], 1), // broadcasts when starting a new duty
+				},
+				ExpectedError: "no consensus phase for validator registration",
+			},
+		},
+	}
+}
