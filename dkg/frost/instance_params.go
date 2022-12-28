c7324286eb41667817ff72b5f08e18580c656e8e
Aayush Gautam <aayush@rockx.com>
2022-12-28 10:21:58 +0800

    add field level comments to state and instance Params

--- dkg/frost/instance_params.go
+++ dkg/frost/instance_params.go
@@ -8,11 +8,18 @@ import (
 // InstanceParams contains properties needed to start the protocol like requestID,
 // operatorID, threshold, operator list etc.
 type InstanceParams struct {
-	identifier      dkg.RequestID
-	threshold       uint32
-	operatorID      types.OperatorID
-	operators       []uint32
-	operatorsOld    []uint32
+	// unique identifier for this instance of keygen or resharing
+	identifier dkg.RequestID
+	// minimum number of operators require to sign a message
+	threshold uint32
+	// operator ID for which this instance is running
+	operatorID types.OperatorID
+	// list of all the operators in the keygen. New committee in case of resharing
+	operators []uint32
+	// list of operators from old committee in case of resharing. Atleast
+	// t (from old keygen) number of operators required to proceed with resharing.
+	operatorsOld []uint32
+	// Keygen output from old keygen instance from this operator in old committee
 	oldKeyGenOutput *dkg.KeyGenOutput
 }
 
