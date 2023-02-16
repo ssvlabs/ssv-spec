ea828c10eac89ab926a847fbef505c36d1aef329
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2023-02-16 15:11:55 +0400

    Fix startinstance.EmptyValue, startinstance.NilValue, startinstance.Valid

--- qbft/spectest/tests/startinstance/valid.go
+++ qbft/spectest/tests/startinstance/valid.go
@@ -13,7 +13,7 @@ func Valid() *tests.ControllerSpecTest {
 		RunInstanceData: []*tests.RunInstanceData{
 			{
 				InputValue:         []byte{1, 2, 3, 4},
-				ControllerPostRoot: "6bd17213f8e308190c4ebe49a22ec00c91ffd4c91a5515583391e9977423370f",
+				ControllerPostRoot: "47713c38fe74ce55959980781287886c603c2117a14dc8abce24dcb9be0093af",
 				ExpectedTimerState: &testingutils.TimerState{
 					Timeouts: 1,
 					Round:    qbft.FirstRound,
