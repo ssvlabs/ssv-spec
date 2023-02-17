bf588dabccb254679359f3c9beae7517e60caf23
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2023-02-17 17:05:18 +0400

    Add proposal.InvalidFullData test

--- qbft/spectest/tests/proposal/invalid_full_data.go
+++ qbft/spectest/tests/proposal/invalid_full_data.go
@@ -1,8 +1,24 @@
 package proposal
 
-import "github.com/bloxapp/ssv-spec/qbft/spectest/tests"
+import (
+	"github.com/bloxapp/ssv-spec/qbft"
+	"github.com/bloxapp/ssv-spec/qbft/spectest/tests"
+	"github.com/bloxapp/ssv-spec/types"
+	"github.com/bloxapp/ssv-spec/types/testingutils"
+)
 
 // InvalidFullData tests signed commit with an invalid full data field (H(full data) != root)
 func InvalidFullData() *tests.MsgProcessingSpecTest {
-	panic("implement")
+	pre := testingutils.BaseInstance()
+	ks := testingutils.Testing4SharesSet()
+	msg := testingutils.TestingProposalMessage(ks.Shares[1], types.OperatorID(1))
+	msg.FullData = nil
+
+	return &tests.MsgProcessingSpecTest{
+		Name:          "invalid full data",
+		Pre:           pre,
+		PostRoot:      "5b18ca0b470208d8d247543306850618f02bddcbaa7c37eb6d5b36eb3accb5fb",
+		InputMessages: []*qbft.SignedMessage{msg},
+		ExpectedError: "invalid signed message: H(data) != root",
+	}
 }
