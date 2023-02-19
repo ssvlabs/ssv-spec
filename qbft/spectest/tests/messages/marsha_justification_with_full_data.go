0178aff3ae77f8e9ed205885a39fb9b528e23f00
Alon Muroch <alonmuroch@gmail.com>
2023-02-19 07:02:29 +0200

    remove full data on marshaling justifications + tests

---
+++ qbft/spectest/tests/messages/marsha_justification_with_full_data.go
@@ -0,0 +1,8 @@
+package messages
+
+import "github.com/bloxapp/ssv-spec/qbft/spectest/tests"
+
+// MarshaJustificationsWithFullData tests marshalling justifications with full data (should omit it)
+func MarshaJustificationsWithFullData() *tests.MsgSpecTest {
+	panic("implement")
+}
