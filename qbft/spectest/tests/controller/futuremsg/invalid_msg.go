a6e1ea1c051a324970bcb1c7fd61404f55dc926f
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2023-02-20 18:40:21 +0400

    Fix futuremsg.InvalidMsg

--- qbft/spectest/tests/controller/futuremsg/invalid_msg.go
+++ qbft/spectest/tests/controller/futuremsg/invalid_msg.go
@@ -2,15 +2,24 @@ package futuremsg
 
 import (
 	"github.com/bloxapp/ssv-spec/qbft"
+	"github.com/bloxapp/ssv-spec/types"
 	"github.com/bloxapp/ssv-spec/types/testingutils"
 )
 
 // InvalidMsg tests future msg invalid msg
 func InvalidMsg() *ControllerSyncSpecTest {
 	ks := testingutils.Testing4SharesSet()
+	identifier := types.NewMsgID(testingutils.TestingSSVDomainType, testingutils.TestingValidatorPubKey[:], types.BNRoleAttester)
 
-	msg := testingutils.TestingPrepareMessageWithParams(ks.Shares[3], 3, 3, 10, testingutils.TestingQBFTRootData)
-	msg.Signature = nil
+	msg := testingutils.SignQBFTMsg(ks.Shares[3], 3, &qbft.Message{
+		MsgType:    qbft.PrepareMsgType,
+		Height:     10,
+		Round:      3,
+		Identifier: identifier[:],
+		Root:       testingutils.TestingQBFTRootData,
+	})
+
+	msg.Signers = nil
 
 	return &ControllerSyncSpecTest{
 		Name: "future invalid msg",
@@ -18,7 +27,7 @@ func InvalidMsg() *ControllerSyncSpecTest {
 			msg,
 		},
 		SyncDecidedCalledCnt: 0,
-		ControllerPostRoot:   "6bd17213f8e308190c4ebe49a22ec00c91ffd4c91a5515583391e9977423370f",
-		ExpectedError:        "invalid future msg: invalid decided msg: message signature is invalid",
+		ControllerPostRoot:   "3b9cd21ca426a4e9e3188e0c8d931861a8f263636c4c0369da84fe9a99fb2fa5",
+		ExpectedError:        "invalid future msg: invalid decided msg: message signers is empty",
 	}
 }
