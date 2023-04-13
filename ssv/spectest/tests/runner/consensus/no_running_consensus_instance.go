834853cc9095a3dd6347004335cb9341ca1a17a7
Alon Muroch <alonmuroch@gmail.com>
2023-04-13 12:08:49 +0300

    fix no running instance test

--- ssv/spectest/tests/runner/consensus/no_running_consensus_instance.go
+++ ssv/spectest/tests/runner/consensus/no_running_consensus_instance.go
@@ -8,13 +8,9 @@ import (
 )
 
 // NoRunningConsensusInstance tests a valid proposal msg before consensus instance starts
+// with pre-consensus justifications there will always be an instance, it's either a future msg or has pre-consensus justification (which will start an instance)
 func NoRunningConsensusInstance() *tests.MultiMsgProcessingSpecTest {
 	ks := testingutils.Testing4SharesSet()
-
-	// register runners and return roots
-	roots := noRunningConsensusInstanceStateComparison().Register().Roots()
-
-	expectedErr := "failed processing consensus message: instance not found"
 	return &tests.MultiMsgProcessingSpecTest{
 		Name: "consensus no running consensus instance",
 		Tests: []*tests.MsgProcessingSpecTest{
@@ -34,10 +30,10 @@ func NoRunningConsensusInstance() *tests.MultiMsgProcessingSpecTest {
 						),
 						nil),
 				},
-				PostDutyRunnerStateRoot: roots[0], // "b2e883cc81caaed04f3e40e8561ae55aa1f6abcdb3168e5cc5c834b1d327026e",
-				OutputMessages:          []*types.SignedPartialSignatureMessage{},
-				DontStartDuty:           true,
-				ExpectedError:           expectedErr,
+				PostDutyRunnerStateRoot: "b2e883cc81caaed04f3e40e8561ae55aa1f6abcdb3168e5cc5c834b1d327026e",
+				OutputMessages: []*types.SignedPartialSignatureMessage{
+					testingutils.PreConsensusContributionProofMsg(ks.Shares[1], ks.Shares[1], 1, 1),
+				},
 			},
 			{
 				Name:   "sync committee",
@@ -51,10 +47,8 @@ func NoRunningConsensusInstance() *tests.MultiMsgProcessingSpecTest {
 						),
 						nil),
 				},
-				PostDutyRunnerStateRoot: roots[1], // "5adbf2c86193070a8f74596275e7a62d48a6a573259150d7ec694b3571c7a787",
+				PostDutyRunnerStateRoot: "5adbf2c86193070a8f74596275e7a62d48a6a573259150d7ec694b3571c7a787",
 				OutputMessages:          []*types.SignedPartialSignatureMessage{},
-				DontStartDuty:           true,
-				ExpectedError:           expectedErr,
 			},
 			{
 				Name:   "aggregator",
@@ -72,10 +66,10 @@ func NoRunningConsensusInstance() *tests.MultiMsgProcessingSpecTest {
 						),
 						nil),
 				},
-				PostDutyRunnerStateRoot: roots[2], // "6e1095601c6fbbd6ba5912dfe296b50db2ae67d4115bce7aa2ad0b091c693ea5",
-				OutputMessages:          []*types.SignedPartialSignatureMessage{},
-				DontStartDuty:           true,
-				ExpectedError:           expectedErr,
+				PostDutyRunnerStateRoot: "6e1095601c6fbbd6ba5912dfe296b50db2ae67d4115bce7aa2ad0b091c693ea5",
+				OutputMessages: []*types.SignedPartialSignatureMessage{
+					testingutils.PreConsensusSelectionProofMsg(ks.Shares[1], ks.Shares[1], 1, 1),
+				},
 			},
 			{
 				Name:   "proposer",
@@ -93,10 +87,10 @@ func NoRunningConsensusInstance() *tests.MultiMsgProcessingSpecTest {
 						),
 						nil),
 				},
-				PostDutyRunnerStateRoot: roots[3], // "a70427708a0ab6995225538b39e7de5cb622af9651fb02a162c6bfbdf5d0966d",
-				OutputMessages:          []*types.SignedPartialSignatureMessage{},
-				DontStartDuty:           true,
-				ExpectedError:           expectedErr,
+				PostDutyRunnerStateRoot: "a70427708a0ab6995225538b39e7de5cb622af9651fb02a162c6bfbdf5d0966d",
+				OutputMessages: []*types.SignedPartialSignatureMessage{
+					testingutils.PreConsensusRandaoMsg(ks.Shares[1], 1),
+				},
 			},
 			{
 				Name:   "proposer (blinded block)",
@@ -114,10 +108,10 @@ func NoRunningConsensusInstance() *tests.MultiMsgProcessingSpecTest {
 						),
 						nil),
 				},
-				PostDutyRunnerStateRoot: roots[4], // "dc9ee0b1b1d1562763855898c9962957bc5d4f3090890419c22e0162705e9ca0",
-				OutputMessages:          []*types.SignedPartialSignatureMessage{},
-				DontStartDuty:           true,
-				ExpectedError:           expectedErr,
+				PostDutyRunnerStateRoot: "dc9ee0b1b1d1562763855898c9962957bc5d4f3090890419c22e0162705e9ca0",
+				OutputMessages: []*types.SignedPartialSignatureMessage{
+					testingutils.PreConsensusRandaoMsg(ks.Shares[1], 1),
+				},
 			},
 			{
 				Name:   "attester",
@@ -131,10 +125,8 @@ func NoRunningConsensusInstance() *tests.MultiMsgProcessingSpecTest {
 						),
 						nil),
 				},
-				PostDutyRunnerStateRoot: roots[5], // "0d5b671f94eeddcb00025dd70fa52d259cafaa5f284645db4fd20e943e2e900d",
+				PostDutyRunnerStateRoot: "0d5b671f94eeddcb00025dd70fa52d259cafaa5f284645db4fd20e943e2e900d",
 				OutputMessages:          []*types.SignedPartialSignatureMessage{},
-				DontStartDuty:           true,
-				ExpectedError:           expectedErr,
 			},
 		},
 	}
