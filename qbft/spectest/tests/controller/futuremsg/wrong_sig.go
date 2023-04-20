36926dd1cef19501b7ba75877b5a198cfc386529
olegshmuelov <45327364+olegshmuelov@users.noreply.github.com>
2023-04-20 15:14:45 +0300

    wrong sig test

--- qbft/spectest/tests/controller/futuremsg/wrong_sig.go
+++ qbft/spectest/tests/controller/futuremsg/wrong_sig.go
@@ -2,6 +2,7 @@ package futuremsg
 
 import (
 	"github.com/bloxapp/ssv-spec/qbft"
+	qbftcomparable "github.com/bloxapp/ssv-spec/qbft/spectest/comparable"
 	"github.com/bloxapp/ssv-spec/qbft/spectest/tests"
 	"github.com/bloxapp/ssv-spec/types"
 	"github.com/bloxapp/ssv-spec/types/testingutils"
@@ -19,7 +20,24 @@ func WrongSig() tests.SpecTest {
 				identifier[:], testingutils.TestingQBFTRootData),
 		},
 		SyncDecidedCalledCnt: 0,
-		ControllerPostRoot:   "3b9cd21ca426a4e9e3188e0c8d931861a8f263636c4c0369da84fe9a99fb2fa5",
+		ControllerPostRoot:   wrongSigStateComparison().Register().Root(),
 		ExpectedError:        "invalid future msg: msg signature invalid: failed to verify signature",
 	}
+}
+
+func wrongSigStateComparison() *qbftcomparable.StateComparison {
+	identifier := types.NewMsgID(testingutils.TestingSSVDomainType, testingutils.TestingValidatorPubKey[:], types.BNRoleAttester)
+	config := testingutils.TestingConfig(testingutils.Testing4SharesSet())
+	contr := testingutils.NewTestingQBFTController(
+		identifier[:],
+		testingutils.TestingShare(testingutils.Testing4SharesSet()),
+		config,
+	)
+	_ = contr.StartNewInstance([]byte{1, 2, 3, 4})
+
+	state := testingutils.BaseInstance().State
+	state.ID = identifier[:]
+	contr.StoredInstances[0].State = state
+
+	return &qbftcomparable.StateComparison{ExpectedState: contr}
 }
