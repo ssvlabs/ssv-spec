afe81a9c0edf6c957c7ec3ea3c7f5aca4a2c0b8a
olegshmuelov <45327364+olegshmuelov@users.noreply.github.com>
2023-04-23 12:16:10 +0300

    late_proposal test

--- qbft/spectest/tests/controller/latemsg/late_proposal.go
+++ qbft/spectest/tests/controller/latemsg/late_proposal.go
@@ -1,11 +1,13 @@
 package latemsg
 
 import (
+	"github.com/herumi/bls-eth-go-binary/bls"
+
 	"github.com/bloxapp/ssv-spec/qbft"
+	qbftcomparable "github.com/bloxapp/ssv-spec/qbft/spectest/comparable"
 	"github.com/bloxapp/ssv-spec/qbft/spectest/tests"
 	"github.com/bloxapp/ssv-spec/types"
 	"github.com/bloxapp/ssv-spec/types/testingutils"
-	"github.com/herumi/bls-eth-go-binary/bls"
 )
 
 // LateProposal tests process late proposal msg for an instance which just decided
@@ -30,9 +32,55 @@ func LateProposal() tests.SpecTest {
 						[]types.OperatorID{1, 2, 3},
 					),
 				},
-				ControllerPostRoot: "24cf697092529cfab3ab06b969d8696692c8bcbb9f41a954f71dc74c3b1d7e97",
+				ControllerPostRoot: lateProposalStateComparison().Register().Root(),
 			},
 		},
 		ExpectedError: "could not process msg: invalid signed message: proposal is not valid with current state",
 	}
+}
+
+func lateProposalStateComparison() *qbftcomparable.StateComparison {
+	identifier := []byte{1, 2, 3, 4}
+	config := testingutils.TestingConfig(testingutils.Testing4SharesSet())
+	contr := testingutils.NewTestingQBFTController(
+		identifier[:],
+		testingutils.TestingShare(testingutils.Testing4SharesSet()),
+		config,
+	)
+	_ = contr.StartNewInstance([]byte{1, 2, 3, 4})
+
+	ks := testingutils.Testing4SharesSet()
+	msgs := testingutils.DecidingMsgsForHeightWithRoot(testingutils.TestingQBFTRootData,
+		testingutils.TestingQBFTFullData, testingutils.TestingIdentifier, qbft.FirstHeight, ks)
+
+	state := testingutils.BaseInstance().State
+	state.ProposalAcceptedForCurrentRound = testingutils.TestingProposalMessage(ks.Shares[1], types.OperatorID(1))
+	state.LastPreparedRound = 1
+	state.LastPreparedValue = testingutils.TestingQBFTFullData
+	state.Decided = true
+	state.DecidedValue = testingutils.TestingQBFTFullData
+
+	state.ProposeContainer = &qbft.MsgContainer{Msgs: map[qbft.Round][]*qbft.SignedMessage{
+		qbft.FirstRound: {
+			msgs[0],
+		},
+	}}
+	state.PrepareContainer = &qbft.MsgContainer{Msgs: map[qbft.Round][]*qbft.SignedMessage{
+		qbft.FirstRound: {
+			msgs[1],
+			msgs[2],
+			msgs[3],
+		},
+	}}
+	state.CommitContainer = &qbft.MsgContainer{Msgs: map[qbft.Round][]*qbft.SignedMessage{
+		qbft.FirstRound: {
+			msgs[4],
+			msgs[5],
+			msgs[6],
+		},
+	}}
+
+	contr.StoredInstances[0].State = state
+
+	return &qbftcomparable.StateComparison{ExpectedState: contr}
 }
