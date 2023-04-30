8f827ead678dc742d18fe75f3d7148e5b7dfd6c4
Alon Muroch <alonmuroch@gmail.com>
2023-04-30 17:58:32 +0300

    Pre cons liveness (#186)
    * copied all changes
    
    * lint fix
    
    * merge fixes
    
    * fixed merge first height future message
    
    * fix no running instance test
    
    * spec test naming issue resolved
    
    * lint fix
    
    * TMP - faster ssv tests
    
    * fixes
    
    * all tests pass + json
    
    * last few fixes
    
    * move isFutureMessage outside of func
    
    * Update qbft/spectest/tests/controller/futuremsg/first_height_not_started.go
    
    Co-authored-by: Gal Rogozinski <galrogogit@gmail.com>
    
    * Update qbft/spectest/tests/controller/futuremsg/first_height_started.go
    
    Co-authored-by: Gal Rogozinski <galrogogit@gmail.com>
    
    * remvoe cosnensus/no_running_consensus_instance test
    
    In the past it tested cases in which a consensus message is received but no running qbft instance is present.
    After a few changes in the code (over the past few months) in such cases it will either be a future message or carrying a pre-consensus in which case a qbft instance will be started.
    
    Essentially it doesn't test anything anymore
    
    * bundled fixes from review
    
    * highest decided comparison readability
    
    * renaming func
    
    * invalid data func comment
    
    * merge fixes
    
    * fix expected error
    
    * Update ssv/spectest/tests/runner/pre_consensus_justifications/invalid_msg_type.go
    
    * Update ssv/spectest/tests/runner/pre_consensus_justifications/invalid_slot.go
    
    * fix comment of auto suggestion
    
    ---------
    
    Co-authored-by: Gal Rogozinski <galrogogit@gmail.com>

--- qbft/spectest/tests/messages/marsha_justification.go
+++
@@ -1,8 +0,0 @@
-package messages
-
-import "github.com/bloxapp/ssv-spec/qbft/spectest/tests"
-
-// MarshaJustifications tests marshalling justifications
-func MarshaJustifications() tests.SpecTest {
-	panic("implement")
-}
