3c97130df49e6d43fe077af4a9c7f43feb00a45e
olegshmuelov <45327364+olegshmuelov@users.noreply.github.com>
2023-05-01 12:58:56 +0300

    get rid of RootRegister map global state

--- qbft/spectest/comparable/types.go
+++ qbft/spectest/comparable/types.go
@@ -6,21 +6,8 @@ import (
 	"github.com/bloxapp/ssv-spec/types"
 )
 
-// RootRegister is a global registry of expected state roots. e.g. {"0x123": &State{}}
-var RootRegister = map[string]*types.Root{}
-
 type StateComparison struct {
 	ExpectedState types.Root
-}
-
-// Register will register state roots with a global registry to be compared against
-func (stateComp *StateComparison) Register() *StateComparison {
-	r, err := stateComp.ExpectedState.GetRoot()
-	if err != nil {
-		panic(err.Error())
-	}
-	RootRegister[hex.EncodeToString(r[:])] = &stateComp.ExpectedState
-	return stateComp
 }
 
 // Root returns all runner roots as string
