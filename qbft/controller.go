dd95f8a40b02f0e1c45c7a262c0c9e3d4d7f3ac4
olegshmuelov <45327364+olegshmuelov@users.noreply.github.com>
2023-05-01 15:50:50 +0300

    use Root interface everywhere possible

--- qbft/controller.go
+++ qbft/controller.go
@@ -5,8 +5,10 @@ import (
 	"crypto/sha256"
 	"encoding/json"
 	"fmt"
-	"github.com/bloxapp/ssv-spec/types"
+
 	"github.com/pkg/errors"
+
+	"github.com/bloxapp/ssv-spec/types"
 )
 
 // Controller is a QBFT coordinator responsible for starting and following the entire life cycle of multiple QBFT InstanceContainer
@@ -171,13 +173,13 @@ func (c *Controller) CanStartInstance() error {
 }
 
 // GetRoot returns the state's deterministic root
-func (c *Controller) GetRoot() ([]byte, error) {
+func (c *Controller) GetRoot() ([32]byte, error) {
 	marshaledRoot, err := json.Marshal(c)
 	if err != nil {
-		return nil, errors.Wrap(err, "could not encode state")
+		return [32]byte{}, errors.Wrap(err, "could not encode state")
 	}
 	ret := sha256.Sum256(marshaledRoot)
-	return ret[:], nil
+	return ret, nil
 }
 
 // Encode implementation
