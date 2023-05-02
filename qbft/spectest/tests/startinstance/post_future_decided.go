a13c91e434328f1f9c2c8d1a9879ccdde0e6668a
olegshmuelov <45327364+olegshmuelov@users.noreply.github.com>
2023-05-02 17:58:08 +0300

    post_future_decided test

--- qbft/spectest/tests/startinstance/post_future_decided.go
+++ qbft/spectest/tests/startinstance/post_future_decided.go
@@ -2,7 +2,9 @@ package startinstance
 
 import (
 	"github.com/bloxapp/ssv-spec/qbft"
+	qbftcomparable "github.com/bloxapp/ssv-spec/qbft/spectest/comparable"
 	"github.com/bloxapp/ssv-spec/qbft/spectest/tests"
+	"github.com/bloxapp/ssv-spec/types"
 	"github.com/bloxapp/ssv-spec/types/testingutils"
 )
 
@@ -21,12 +23,72 @@ func PreviousDecided() tests.SpecTest {
 					DecidedVal: testingutils.TestingQBFTFullData,
 					DecidedCnt: 1,
 				},
-				ControllerPostRoot: "24cf697092529cfab3ab06b969d8696692c8bcbb9f41a954f71dc74c3b1d7e97",
+				ControllerPostRoot:  previousDecidedStateComparison(qbft.FirstHeight, true).Root(),
+				ControllerPostState: previousDecidedStateComparison(qbft.FirstHeight, true).ExpectedState,
 			},
 			{
-				InputValue:         []byte{1, 2, 3, 4},
-				ControllerPostRoot: "5c23b58681df24d3131dab8fcff377035d03bb5a8db99c16c68bd644baa2d3b3",
+				InputValue:          []byte{1, 2, 3, 4},
+				ControllerPostRoot:  previousDecidedStateComparison(1, false).Root(),
+				ControllerPostState: previousDecidedStateComparison(1, false).ExpectedState,
 			},
 		},
 	}
+}
+
+func previousDecidedStateComparison(height qbft.Height, decidedState bool) *qbftcomparable.StateComparison {
+	identifier := []byte{1, 2, 3, 4}
+	config := testingutils.TestingConfig(testingutils.Testing4SharesSet())
+	contr := testingutils.NewTestingQBFTController(
+		identifier[:],
+		testingutils.TestingShare(testingutils.Testing4SharesSet()),
+		config,
+	)
+
+	ks := testingutils.Testing4SharesSet()
+	msgs := testingutils.DecidingMsgsForHeightWithRoot(testingutils.TestingQBFTRootData,
+		testingutils.TestingQBFTFullData, testingutils.TestingIdentifier, qbft.FirstHeight, ks)
+
+	for i := 0; i <= int(height); i++ {
+		contr.Height = qbft.Height(i)
+		_ = contr.StartNewInstance([]byte{1, 2, 3, 4})
+
+		state := testingutils.BaseInstance().State
+		state.Height = qbft.Height(i)
+
+		// last height
+		if !decidedState && i == int(height) {
+			contr.StoredInstances[0].State = state
+			break
+		}
+
+		state.ProposalAcceptedForCurrentRound = testingutils.TestingProposalMessageWithParams(ks.Shares[1], types.OperatorID(1), qbft.FirstRound, qbft.Height(i), testingutils.TestingQBFTRootData, nil, nil)
+		state.ProposalAcceptedForCurrentRound.Message.Height = qbft.Height(i)
+		state.LastPreparedRound = 1
+		state.LastPreparedValue = testingutils.TestingQBFTFullData
+		state.Decided = true
+		state.DecidedValue = testingutils.TestingQBFTFullData
+		state.ProposeContainer = &qbft.MsgContainer{Msgs: map[qbft.Round][]*qbft.SignedMessage{
+			qbft.FirstRound: {
+				msgs[0],
+			},
+		}}
+		state.PrepareContainer = &qbft.MsgContainer{Msgs: map[qbft.Round][]*qbft.SignedMessage{
+			qbft.FirstRound: {
+				msgs[1],
+				msgs[2],
+				msgs[3],
+			},
+		}}
+		state.CommitContainer = &qbft.MsgContainer{Msgs: map[qbft.Round][]*qbft.SignedMessage{
+			qbft.FirstRound: {
+				msgs[4],
+				msgs[5],
+				msgs[6],
+			},
+		}}
+
+		contr.StoredInstances[0].State = state
+	}
+
+	return &qbftcomparable.StateComparison{ExpectedState: contr}
 }
