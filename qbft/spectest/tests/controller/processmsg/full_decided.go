f1a015731998e9a87f55ecc399adbecf35cb64f9
olegshmuelov <45327364+olegshmuelov@users.noreply.github.com>
2023-05-02 15:43:55 +0300

    full_decided test

--- qbft/spectest/tests/controller/processmsg/full_decided.go
+++ qbft/spectest/tests/controller/processmsg/full_decided.go
@@ -1,16 +1,19 @@
 package processmsg
 
 import (
+	"github.com/herumi/bls-eth-go-binary/bls"
+
 	"github.com/bloxapp/ssv-spec/qbft"
+	qbftcomparable "github.com/bloxapp/ssv-spec/qbft/spectest/comparable"
 	"github.com/bloxapp/ssv-spec/qbft/spectest/tests"
 	"github.com/bloxapp/ssv-spec/types"
 	"github.com/bloxapp/ssv-spec/types/testingutils"
-	"github.com/herumi/bls-eth-go-binary/bls"
 )
 
 // FullDecided tests process msg and first time deciding
 func FullDecided() tests.SpecTest {
 	ks := testingutils.Testing4SharesSet()
+	sc := fullDecidedStateComparison()
 	return &tests.ControllerSpecTest{
 		Name: "full decided",
 		RunInstanceData: []*tests.RunInstanceData{
@@ -26,8 +29,55 @@ func FullDecided() tests.SpecTest {
 						[]types.OperatorID{1, 2, 3},
 					),
 				},
-				ControllerPostRoot: "4c96913e87aa17c9f0d5c1b6b220cbc7a66b7b40ef55f1059f1e1fa9f59c94d9",
+				ControllerPostRoot:  sc.Root(),
+				ControllerPostState: sc.ExpectedState,
 			},
 		},
 	}
+}
+
+func fullDecidedStateComparison() *qbftcomparable.StateComparison {
+	identifier := []byte{1, 2, 3, 4}
+	config := testingutils.TestingConfig(testingutils.Testing4SharesSet())
+	contr := testingutils.NewTestingQBFTController(
+		identifier[:],
+		testingutils.TestingShare(testingutils.Testing4SharesSet()),
+		config,
+	)
+	_ = contr.StartNewInstance(testingutils.TestingQBFTFullData)
+
+	ks := testingutils.Testing4SharesSet()
+	msgs := testingutils.DecidingMsgsForHeightWithRoot(testingutils.TestingQBFTRootData,
+		testingutils.TestingQBFTFullData, testingutils.TestingIdentifier, qbft.FirstHeight, ks)
+
+	state := testingutils.BaseInstance().State
+	state.ProposalAcceptedForCurrentRound = testingutils.TestingProposalMessage(ks.Shares[1], types.OperatorID(1))
+	state.LastPreparedRound = 1
+	state.LastPreparedValue = testingutils.TestingQBFTFullData
+	state.Decided = true
+	state.DecidedValue = testingutils.TestingQBFTFullData
+
+	state.ProposeContainer = &qbft.MsgContainer{Msgs: map[qbft.Round][]*qbft.SignedMessage{
+		qbft.FirstRound: {
+			msgs[0],
+		},
+	}}
+	state.PrepareContainer = &qbft.MsgContainer{Msgs: map[qbft.Round][]*qbft.SignedMessage{
+		qbft.FirstRound: {
+			msgs[1],
+			msgs[2],
+			msgs[3],
+		},
+	}}
+	state.CommitContainer = &qbft.MsgContainer{Msgs: map[qbft.Round][]*qbft.SignedMessage{
+		qbft.FirstRound: {
+			msgs[4],
+			msgs[5],
+			msgs[6],
+		},
+	}}
+
+	contr.StoredInstances[0].State = state
+
+	return &qbftcomparable.StateComparison{ExpectedState: contr}
 }
