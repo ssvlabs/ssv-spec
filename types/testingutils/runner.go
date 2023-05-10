542d683a063488f231c4415cb86001c18790e4e7
olegshmuelov <45327364+olegshmuelov@users.noreply.github.com>
2023-05-10 11:29:13 +0300

    remove non-versioned funcs

--- types/testingutils/runner.go
+++ types/testingutils/runner.go
@@ -2,6 +2,7 @@ package testingutils
 
 import (
 	"github.com/attestantio/go-eth2-client/spec/phase0"
+
 	"github.com/bloxapp/ssv-spec/qbft"
 	"github.com/bloxapp/ssv-spec/ssv"
 	"github.com/bloxapp/ssv-spec/types"
@@ -181,53 +182,6 @@ var decideRunner = func(consensusInput *types.ConsensusData, height qbft.Height,
 	}
 
 	return v.DutyRunners[types.BNRoleAttester]
-}
-
-var SSVDecidingMsgs = func(consensusData *types.ConsensusData, ks *TestKeySet, role types.BeaconRole) []*types.SSVMessage {
-	id := types.NewMsgID(TestingSSVDomainType, TestingValidatorPubKey[:], role)
-
-	ssvMsgF := func(qbftMsg *qbft.SignedMessage, partialSigMsg *types.SignedPartialSignatureMessage) *types.SSVMessage {
-		var byts []byte
-		var msgType types.MsgType
-		if partialSigMsg != nil {
-			msgType = types.SSVPartialSignatureMsgType
-			byts, _ = partialSigMsg.Encode()
-		} else {
-			msgType = types.SSVConsensusMsgType
-			byts, _ = qbftMsg.Encode()
-		}
-
-		return &types.SSVMessage{
-			MsgType: msgType,
-			MsgID:   id,
-			Data:    byts,
-		}
-	}
-
-	// pre consensus msgs
-	base := make([]*types.SSVMessage, 0)
-	if role == types.BNRoleProposer {
-		for i := uint64(1); i <= ks.Threshold; i++ {
-			base = append(base, ssvMsgF(nil, PreConsensusRandaoMsg(ks.Shares[types.OperatorID(i)], types.OperatorID(i))))
-		}
-	}
-	if role == types.BNRoleAggregator {
-		for i := uint64(1); i <= ks.Threshold; i++ {
-			base = append(base, ssvMsgF(nil, PreConsensusSelectionProofMsg(ks.Shares[types.OperatorID(i)], ks.Shares[types.OperatorID(i)], types.OperatorID(i), types.OperatorID(i))))
-		}
-	}
-	if role == types.BNRoleSyncCommitteeContribution {
-		for i := uint64(1); i <= ks.Threshold; i++ {
-			base = append(base, ssvMsgF(nil, PreConsensusContributionProofMsg(ks.Shares[types.OperatorID(i)], ks.Shares[types.OperatorID(i)], types.OperatorID(i), types.OperatorID(i))))
-		}
-	}
-
-	// consensus and post consensus
-	qbftMsgs := DecidingMsgsForHeight(consensusData, id[:], qbft.FirstHeight, ks)
-	for _, msg := range qbftMsgs {
-		base = append(base, ssvMsgF(msg, nil))
-	}
-	return base
 }
 
 var DecidingMsgsForHeight = func(consensusData *types.ConsensusData, msgIdentifier []byte, height qbft.Height, keySet *TestKeySet) []*qbft.SignedMessage {
