f11bc256f44120d8ea9d1d39d8b1b9c8e424c0b9
Alon Muroch <alonmuroch@gmail.com>
2023-05-15 09:44:00 +0300

    cleanup and comments

--- types/chains.go
+++ types/chains.go
@@ -5,23 +5,28 @@ import "github.com/attestantio/go-eth2-client/spec/phase0"
 // DomainType is a unique identifier for signatures, 2 identical pieces of data signed with different domains will result in different sigs
 type DomainType [4]byte
 
+// Chains are intended to separate different SSV chains. A chain can have many forks in it.
 const (
-	GenesisChain = 0x0
+	MainnetChain = 0x0
 	PrimusChain  = 0x1
 	ShifuChain   = 0x2
 	JatoChain    = 0x3
 )
 
+// DomainTypes represent specific forks for specific chains, messages are signed with the domain type making 2 messages from different domains incompatible
 var (
-	GenesisMainnet = DomainType{0x0, 0x0, GenesisChain, 0x0}
+	GenesisMainnet = DomainType{0x0, 0x0, MainnetChain, 0x0}
 	PrimusTestnet  = DomainType{0x0, 0x0, PrimusChain, 0x0}
 	ShifuTestnet   = DomainType{0x0, 0x0, ShifuChain, 0x0}
 	ShifuV2Testnet = DomainType{0x0, 0x0, ShifuChain, 0x1}
 	JatoTestnet    = DomainType{0x0, 0x0, JatoChain, 0x1}
 )
 
+// ForkData is a simple structure holding fork information for a specific chain (and its fork)
 type ForkData struct {
-	Epoch  phase0.Epoch
+	// Epoch in which the fork happened
+	Epoch phase0.Epoch
+	// Domain for the new fork
 	Domain DomainType
 }
 
@@ -31,14 +36,15 @@ func (domainType DomainType) GetChain() byte {
 
 func (domainType DomainType) GetForksData() []*ForkData {
 	switch domainType.GetChain() {
-	case GenesisChain:
-		return genesisForks()
+	case MainnetChain:
+		return mainnetForks()
 	default:
 		return []*ForkData{}
 	}
 }
 
-func genesisForks() []*ForkData {
+// mainnetForks returns all forks for the mainnet chain
+func mainnetForks() []*ForkData {
 	return []*ForkData{
 		{
 			Epoch:  0,
