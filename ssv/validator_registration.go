1c13418cb33396f52488e7851af75403e6b59190
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2023-05-15 15:50:53 +0400

    Remove a wrong comment

--- ssv/validator_registration.go
+++ ssv/validator_registration.go
@@ -63,7 +63,6 @@ func (r *ValidatorRegistrationRunner) ProcessPreConsensus(signedMsg *types.Signe
 
 	// only 1 root, verified in basePreConsensusMsgProcessing
 	root := roots[0]
-	// randao is relevant only for block proposals, no need to check type
 	fullSig, err := r.GetState().ReconstructBeaconSig(r.GetState().PreConsensusContainer, root, r.GetShare().ValidatorPubKey)
 	if err != nil {
 		return errors.Wrap(err, "could not reconstruct randao sig")
