d21efa723e2a54aacef0e33f76a4f663967ad5b5
olegshmuelov <45327364+olegshmuelov@users.noreply.github.com>
2023-05-21 17:29:51 +0300

    preconsensus/no_running_duty full sc test

--- ssv/spectest/tests/runner/preconsensus/no_running_duty_sc.go
+++ ssv/spectest/tests/runner/preconsensus/no_running_duty_sc.go
@@ -8,6 +8,39 @@ import (
 	"github.com/bloxapp/ssv-spec/types/testingutils/comparable"
 )
 
+// noRunningDutySyncCommitteeContributionSC returns state comparison object for the NoRunningDuty SyncCommitteeContribution versioned spec test
+func noRunningDutySyncCommitteeContributionSC() *comparable.StateComparison {
+	ks := testingutils.Testing4SharesSet()
+
+	return &comparable.StateComparison{
+		ExpectedState: func() types.Root {
+			return testingutils.SyncCommitteeContributionRunner(ks)
+		}(),
+	}
+}
+
+// noRunningDutyAggregatorSC returns state comparison object for the NoRunningDuty Aggregator versioned spec test
+func noRunningDutyAggregatorSC() *comparable.StateComparison {
+	ks := testingutils.Testing4SharesSet()
+
+	return &comparable.StateComparison{
+		ExpectedState: func() types.Root {
+			return testingutils.AggregatorRunner(ks)
+		}(),
+	}
+}
+
+// noRunningDutyValidatorRegistrationSC returns state comparison object for the NoRunningDuty ValidatorRegistration versioned spec test
+func noRunningDutyValidatorRegistrationSC() *comparable.StateComparison {
+	ks := testingutils.Testing4SharesSet()
+
+	return &comparable.StateComparison{
+		ExpectedState: func() types.Root {
+			return testingutils.ValidatorRegistrationRunner(ks)
+		}(),
+	}
+}
+
 // noRunningDutyProposerSC returns state comparison object for the NoRunningDuty Proposer versioned spec test
 func noRunningDutyProposerSC(version spec.DataVersion) *comparable.StateComparison {
 	ks := testingutils.Testing4SharesSet()
