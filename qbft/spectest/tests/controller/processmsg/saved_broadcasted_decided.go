3b3f023450a74562357eb7a2b485265730a1493f
olegshmuelov <45327364+olegshmuelov@users.noreply.github.com>
2023-06-06 10:31:30 +0300

    State comparison - QBFT controller/processmsg (#209)
    * implementation
    
    * move rootgetter interface to types package
    
    * add sc to controller spectest
    
    * full_decided test
    
    * saved_broadcasted_decided test
    
    * add sc to controller sync spec test
    
    * add sc to controller spectest
    
    * set signed messages functionality
    
    * ExpectedDecidingMsgsForHeightWithRoot
    
    * clean code + expected decided msgs
    
    * fix merge conflicts

--- qbft/spectest/tests/controller/processmsg/saved_broadcasted_decided.go
+++ qbft/spectest/tests/controller/processmsg/saved_broadcasted_decided.go
@@ -1,16 +1,19 @@
 package processmsg
 
 import (
+	"github.com/herumi/bls-eth-go-binary/bls"
+
 	"github.com/bloxapp/ssv-spec/qbft"
 	"github.com/bloxapp/ssv-spec/qbft/spectest/tests"
 	"github.com/bloxapp/ssv-spec/types"
 	"github.com/bloxapp/ssv-spec/types/testingutils"
-	"github.com/herumi/bls-eth-go-binary/bls"
+	"github.com/bloxapp/ssv-spec/types/testingutils/comparable"
 )
 
 // BroadcastedDecided tests broadcasting decided
 func BroadcastedDecided() tests.SpecTest {
 	ks := testingutils.Testing4SharesSet()
+	sc := broadcastedDecidedStateComparison()
 	return &tests.ControllerSpecTest{
 		Name: "broadcast decided",
 		RunInstanceData: []*tests.RunInstanceData{
@@ -26,8 +29,38 @@ func BroadcastedDecided() tests.SpecTest {
 						[]types.OperatorID{1, 2, 3},
 					),
 				},
-				ControllerPostRoot: "24cf697092529cfab3ab06b969d8696692c8bcbb9f41a954f71dc74c3b1d7e97",
+				ControllerPostRoot:  sc.Root(),
+				ControllerPostState: sc.ExpectedState,
 			},
 		},
 	}
+}
+
+func broadcastedDecidedStateComparison() *comparable.StateComparison {
+	ks := testingutils.Testing4SharesSet()
+	msgs := testingutils.ExpectedDecidingMsgsForHeightWithRoot(testingutils.TestingQBFTRootData, testingutils.TestingQBFTFullData, testingutils.TestingIdentifier, qbft.FirstHeight, ks)
+
+	contr := testingutils.NewTestingQBFTController(
+		testingutils.TestingIdentifier,
+		testingutils.TestingShare(testingutils.Testing4SharesSet()),
+		testingutils.TestingConfig(testingutils.Testing4SharesSet()),
+	)
+
+	instance := &qbft.Instance{
+		StartValue: []byte{1, 2, 3, 4},
+		State: &qbft.State{
+			Share:                           testingutils.TestingShare(testingutils.Testing4SharesSet()),
+			ID:                              testingutils.TestingIdentifier,
+			ProposalAcceptedForCurrentRound: testingutils.TestingProposalMessage(ks.Shares[1], types.OperatorID(1)),
+			LastPreparedRound:               qbft.FirstRound,
+			LastPreparedValue:               testingutils.TestingQBFTFullData,
+			Decided:                         true,
+			DecidedValue:                    testingutils.TestingQBFTFullData,
+			Round:                           qbft.FirstRound,
+		},
+	}
+	comparable.SetSignedMessages(instance, msgs)
+	contr.StoredInstances = append(contr.StoredInstances, instance)
+
+	return &comparable.StateComparison{ExpectedState: contr}
 }
