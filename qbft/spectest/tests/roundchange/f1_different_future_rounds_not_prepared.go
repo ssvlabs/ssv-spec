d39d1bbd7a97bf6fb5e0c5af84e7aa63a9872f69
olegshmuelov <45327364+olegshmuelov@users.noreply.github.com>
2023-06-06 15:40:38 +0300

    State comparison - QBFT roundchange (#211)
    * implementation
    
    * move rootgetter interface to types package
    
    * duplicate_msg_quorum_prepared_rc_first test
    
    * f1_different_future_rounds test
    
    * f1_different_future_rounds_not_prepared test
    
    * quorum_msg_not_prepared test
    
    * quorum_order1 test
    
    * rc_partial_quorum test
    
    * fix sc
    
    * set signed messages functionality
    
    * inline state
    
    * inline state
    
    * fix merge conflicts

--- qbft/spectest/tests/roundchange/f1_different_future_rounds_not_prepared.go
+++ qbft/spectest/tests/roundchange/f1_different_future_rounds_not_prepared.go
@@ -5,12 +5,14 @@ import (
 	"github.com/bloxapp/ssv-spec/qbft/spectest/tests"
 	"github.com/bloxapp/ssv-spec/types"
 	"github.com/bloxapp/ssv-spec/types/testingutils"
+	"github.com/bloxapp/ssv-spec/types/testingutils/comparable"
 )
 
 // F1DifferentFutureRoundsNotPrepared tests f+1 speedup (not prev prepared)
 func F1DifferentFutureRoundsNotPrepared() tests.SpecTest {
 	pre := testingutils.BaseInstance()
 	ks := testingutils.Testing4SharesSet()
+	sc := f1DifferentFutureRoundsNotPreparedStateComparison()
 
 	msgs := []*qbft.SignedMessage{
 		testingutils.TestingRoundChangeMessageWithRound(ks.Shares[1], types.OperatorID(1), 5),
@@ -20,11 +22,31 @@ func F1DifferentFutureRoundsNotPrepared() tests.SpecTest {
 	return &tests.MsgProcessingSpecTest{
 		Name:          "round change f+1 not prepared",
 		Pre:           pre,
-		PostRoot:      "d8a2e893fc55a2aa3232d2c36a578260efa73cd083eecea1babec33c23725ba4",
+		PostRoot:      sc.Root(),
+		PostState:     sc.ExpectedState,
 		InputMessages: msgs,
 		OutputMessages: []*qbft.SignedMessage{
 			testingutils.TestingRoundChangeMessageWithParams(ks.Shares[1], types.OperatorID(1), 5, qbft.FirstHeight,
 				[32]byte{}, 0, [][]byte{}),
 		},
 	}
+}
+
+func f1DifferentFutureRoundsNotPreparedStateComparison() *comparable.StateComparison {
+	ks := testingutils.Testing4SharesSet()
+
+	msgs := []*qbft.SignedMessage{
+		testingutils.TestingRoundChangeMessageWithRound(ks.Shares[1], types.OperatorID(1), 5),
+		testingutils.TestingRoundChangeMessageWithRound(ks.Shares[2], types.OperatorID(2), 10),
+	}
+
+	instance := &qbft.Instance{
+		State: &qbft.State{
+			Share: testingutils.TestingShare(testingutils.Testing4SharesSet()),
+			ID:    testingutils.TestingIdentifier,
+			Round: 5,
+		},
+	}
+	comparable.SetSignedMessages(instance, msgs)
+	return &comparable.StateComparison{ExpectedState: instance.State}
 }
--- qbft/spectest/tests/roundchange/quorum_order1.go
+++ qbft/spectest/tests/roundchange/quorum_order1.go
@@ -5,12 +5,14 @@ import (
 	"github.com/bloxapp/ssv-spec/qbft/spectest/tests"
 	"github.com/bloxapp/ssv-spec/types"
 	"github.com/bloxapp/ssv-spec/types/testingutils"
+	"github.com/bloxapp/ssv-spec/types/testingutils/comparable"
 )
 
 // QuorumOrder1 tests LIVENESS where the rc quorum msgs in different order
 func QuorumOrder1() tests.SpecTest {
 	pre := testingutils.BaseInstance()
 	pre.State.Round = 2
+	sc := quorumOrder1StateComparison()
 
 	ks := testingutils.Testing4SharesSet()
 	prepareMsgs := []*qbft.SignedMessage{
@@ -28,7 +30,8 @@ func QuorumOrder1() tests.SpecTest {
 	return &tests.MsgProcessingSpecTest{
 		Name:          "round change quorum order 1",
 		Pre:           pre,
-		PostRoot:      "f8fde754bbe247962fcbd9c8890f79f256ee4e5af7aaa13fc5da2ee85b89333c",
+		PostRoot:      sc.Root(),
+		PostState:     sc.ExpectedState,
 		InputMessages: msgs,
 		OutputMessages: []*qbft.SignedMessage{
 			testingutils.TestingProposalMessageWithParams(ks.Shares[1], types.OperatorID(1), 2, qbft.FirstHeight,
@@ -36,4 +39,31 @@ func QuorumOrder1() tests.SpecTest {
 				testingutils.MarshalJustifications(msgs), testingutils.MarshalJustifications(prepareMsgs)),
 		},
 	}
+}
+
+func quorumOrder1StateComparison() *comparable.StateComparison {
+	ks := testingutils.Testing4SharesSet()
+
+	prepareMsgs := []*qbft.SignedMessage{
+		testingutils.TestingPrepareMessage(ks.Shares[1], types.OperatorID(1)),
+		testingutils.TestingPrepareMessage(ks.Shares[3], types.OperatorID(3)),
+		testingutils.TestingPrepareMessage(ks.Shares[2], types.OperatorID(2)),
+	}
+
+	msgs := []*qbft.SignedMessage{
+		testingutils.TestingRoundChangeMessageWithRoundAndRC(ks.Shares[1], types.OperatorID(1), 2,
+			testingutils.MarshalJustifications(prepareMsgs)),
+		testingutils.TestingRoundChangeMessageWithRound(ks.Shares[2], types.OperatorID(2), 2),
+		testingutils.TestingRoundChangeMessageWithRound(ks.Shares[3], types.OperatorID(3), 2),
+	}
+
+	instance := &qbft.Instance{
+		State: &qbft.State{
+			Share: testingutils.TestingShare(testingutils.Testing4SharesSet()),
+			ID:    testingutils.TestingIdentifier,
+			Round: 2,
+		},
+	}
+	comparable.SetSignedMessages(instance, msgs)
+	return &comparable.StateComparison{ExpectedState: instance.State}
 }
