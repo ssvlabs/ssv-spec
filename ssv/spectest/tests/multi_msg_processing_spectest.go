afad9efe4f3539957315739319e0b354644758a5
Alon Muroch <alonmuroch@gmail.com>
2023-06-19 14:32:28 +0300

    ssv multi msg processing test json state comparison

--- ssv/spectest/tests/multi_msg_processing_spectest.go
+++ ssv/spectest/tests/multi_msg_processing_spectest.go
@@ -1,6 +1,14 @@
 package tests
 
 import (
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"github.com/bloxapp/ssv-spec/ssv"
+	"github.com/stretchr/testify/require"
+	"os"
+	"path/filepath"
+	"reflect"
 	"testing"
 )
 
@@ -14,6 +22,8 @@ func (tests *MultiMsgProcessingSpecTest) TestName() string {
 }
 
 func (tests *MultiMsgProcessingSpecTest) Run(t *testing.T) {
+	tests.overrideStateComparison(t)
+
 	for _, test := range tests.Tests {
 		t.Run(test.TestName(), func(t *testing.T) {
 			test.RunAsMultiTest(t)
@@ -21,6 +31,53 @@ func (tests *MultiMsgProcessingSpecTest) Run(t *testing.T) {
 	}
 }
 
+func (tests *MultiMsgProcessingSpecTest) overrideStateComparison(t *testing.T) {
+	basedir, _ := os.Getwd()
+	path := filepath.Join(basedir, "generate", "state_comparison", reflect.TypeOf(tests).String(), fmt.Sprintf("%s.json", tests.TestName()))
+	byteValue, err := os.ReadFile(path)
+	require.NoError(t, err)
+
+	toDecode := make([]ssv.Runner, len(tests.Tests))
+	for i, test := range tests.Tests {
+		var r ssv.Runner
+		switch test.Runner.(type) {
+		case *ssv.AttesterRunner:
+			r = &ssv.AttesterRunner{}
+		case *ssv.AggregatorRunner:
+			r = &ssv.AggregatorRunner{}
+		case *ssv.ProposerRunner:
+			r = &ssv.ProposerRunner{}
+		case *ssv.SyncCommitteeRunner:
+			r = &ssv.SyncCommitteeRunner{}
+		case *ssv.SyncCommitteeAggregatorRunner:
+			r = &ssv.SyncCommitteeAggregatorRunner{}
+		case *ssv.ValidatorRegistrationRunner:
+			r = &ssv.ValidatorRegistrationRunner{}
+		default:
+			t.Fatalf("unknown runner type")
+		}
+		toDecode[i] = r
+	}
+	require.NoError(t, json.Unmarshal(byteValue, &toDecode))
+
+	// override
+	for i, test := range tests.Tests {
+		test.PostDutyRunnerState = toDecode[i]
+
+		r, err := toDecode[i].GetRoot()
+		require.NoError(t, err)
+		test.PostDutyRunnerStateRoot = hex.EncodeToString(r[:])
+	}
+}
+
 func (tests *MultiMsgProcessingSpecTest) GetPostState() (interface{}, error) {
-	return nil, nil
+	ret := make([]ssv.Runner, len(tests.Tests))
+	for i, test := range tests.Tests {
+		_, err := test.runPreTesting()
+		if err != nil && len(test.ExpectedError) == 0 {
+			return nil, err
+		}
+		ret[i] = test.Runner
+	}
+	return ret, nil
 }
