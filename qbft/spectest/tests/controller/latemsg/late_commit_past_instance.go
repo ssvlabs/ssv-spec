a48aeaa6bb2e8da354ac0c8a795c9e299ea4bb10
Gal Rogozinski <galrogogit@gmail.com>
2023-06-22 13:15:35 +0300

    fix old SC - now we don't store late commits in containers

--- qbft/spectest/tests/controller/latemsg/late_commit_past_instance.go
+++ qbft/spectest/tests/controller/latemsg/late_commit_past_instance.go
@@ -103,10 +103,6 @@ func lateCommitPastInstanceStateComparison(height qbft.Height, lateMsg *qbft.Sig
 		msgs := allMsgs[offset*i : offset*(i+1)]
 		comparable.SetSignedMessages(instance, msgs)
 
-		if lateMsg != nil && qbft.Height(i) == lateMsg.Message.Height {
-			instance.State.CommitContainer.Msgs[qbft.FirstRound] = append(instance.State.CommitContainer.Msgs[qbft.FirstRound], lateMsg)
-		}
-
 		contr.StoredInstances = append([]*qbft.Instance{instance}, contr.StoredInstances...)
 	}
 
