6615fbff84ab9170a95038173136686c518c8c83
Gal Rogozinski <galrogogit@gmail.com>
2023-07-04 17:06:15 +0300

    fix lint errors

--- qbft/spectest/tests/startinstance/prev_not_decided.go
+++ qbft/spectest/tests/startinstance/prev_not_decided.go
@@ -3,6 +3,7 @@ package startinstance
 import (
 	"github.com/bloxapp/ssv-spec/qbft"
 	"github.com/bloxapp/ssv-spec/qbft/spectest/tests"
+	"github.com/bloxapp/ssv-spec/types"
 	"github.com/bloxapp/ssv-spec/types/testingutils"
 	qbftcomparable "github.com/bloxapp/ssv-spec/types/testingutils/comparable"
 )
@@ -34,7 +35,18 @@ func previousNotDecided1SC() *qbftcomparable.StateComparison {
 		testingutils.TestingShare(testingutils.Testing4SharesSet()),
 		config,
 	)
-	contr.StartNewInstance(qbft.FirstHeight, []byte{1, 2, 3, 4})
+	instance := &qbft.Instance{
+		State: &qbft.State{
+			Share:             testingutils.TestingShare(testingutils.Testing4SharesSet()),
+			ID:                identifier,
+			Round:             qbft.FirstRound,
+			Height:            qbft.FirstHeight,
+			LastPreparedRound: qbft.NoRound,
+		},
+		StartValue: []byte{1, 2, 3, 4},
+	}
+	qbftcomparable.SetMessages(instance, []*types.SSVMessage{})
+	contr.StoredInstances = append(contr.StoredInstances, instance)
 	return &qbftcomparable.StateComparison{ExpectedState: contr}
 }
 
@@ -46,7 +58,30 @@ func previousNotDecided2SC() *qbftcomparable.StateComparison {
 		testingutils.TestingShare(testingutils.Testing4SharesSet()),
 		config,
 	)
-	contr.StartNewInstance(qbft.FirstHeight, []byte{1, 2, 3, 4})
-	contr.StartNewInstance(1, []byte{1, 2, 3, 4})
+	instance1 := &qbft.Instance{
+		State: &qbft.State{
+			Share:             testingutils.TestingShare(testingutils.Testing4SharesSet()),
+			ID:                identifier,
+			Round:             qbft.FirstRound,
+			Height:            qbft.FirstHeight,
+			LastPreparedRound: qbft.NoRound,
+		},
+		StartValue: []byte{1, 2, 3, 4},
+	}
+	qbftcomparable.SetMessages(instance1, []*types.SSVMessage{})
+
+	instance2 := &qbft.Instance{
+		State: &qbft.State{
+			Share:             testingutils.TestingShare(testingutils.Testing4SharesSet()),
+			ID:                identifier,
+			Round:             qbft.FirstRound,
+			Height:            1,
+			LastPreparedRound: qbft.NoRound,
+		},
+		StartValue: []byte{1, 2, 3, 4},
+	}
+	qbftcomparable.SetMessages(instance2, []*types.SSVMessage{})
+	contr.StoredInstances = []*qbft.Instance{instance2, instance1}
+	contr.Height = 1
 	return &qbftcomparable.StateComparison{ExpectedState: contr}
 }
--- qbft/timeout.go
+++ qbft/timeout.go
@@ -6,9 +6,9 @@ import (
 )
 
 var (
-	quickTimeoutThreshold = Round(8)
-	quickTimeout          = 2 * time.Second
-	slowTimeout           = 2 * time.Minute
+	quickTimeoutThreshold = Round(8)        //nolint
+	quickTimeout          = 2 * time.Second //nolint
+	slowTimeout           = 2 * time.Minute //nolint
 	// CutoffRound which round the instance should stop its timer and progress no further
 	CutoffRound = 15 // stop processing instances after 8*2+120*6 = 14.2 min (~ 2 epochs)
 )
