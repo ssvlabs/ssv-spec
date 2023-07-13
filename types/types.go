07b695a3ba1dfe691d806b613a60094b6dad6787
Gal Rogozinski <galrogogit@gmail.com>
2023-07-13 13:55:13 +0300

    add hexbytes

--- types/types.go
+++ types/types.go
@@ -1,5 +1,64 @@
 package types
 
+import (
+	"encoding/hex"
+	"encoding/json"
+)
+
 type Root interface {
 	GetRoot() ([32]byte, error)
+}
+
+type HexBytes32 [32]byte
+type HexBytes20 [20]byte
+type HexBytes4 [4]byte
+
+// MarshalJSON implements the json.Marshaler interface
+func (h *HexBytes32) MarshalJSON() ([]byte, error) {
+	return marshalJson(h[:])
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface
+func (h *HexBytes32) UnmarshalJSON(b []byte) error {
+	return unmarshalJson(b, h[:])
+}
+
+// MarshalJSON implements the json.Marshaler interface
+func (h *HexBytes20) MarshalJSON() ([]byte, error) {
+	return marshalJson(h[:])
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface
+func (h *HexBytes20) UnmarshalJSON(b []byte) error {
+	return unmarshalJson(b, h[:])
+}
+
+// MarshalJSON implements the json.Marshaler interface
+func (h *HexBytes4) MarshalJSON() ([]byte, error) {
+	return marshalJson(h[:])
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface
+func (h *HexBytes4) UnmarshalJSON(b []byte) error {
+	return unmarshalJson(b, h[:])
+}
+
+func marshalJson(h []byte) ([]byte, error) {
+	dst := make([]byte, hex.EncodedLen(len(h)))
+	hex.Encode(dst, h[:])
+	return json.Marshal(string(dst))
+}
+
+func unmarshalJson(b []byte, h []byte) error {
+	var str string
+	err := json.Unmarshal(b, &str)
+	if err != nil {
+		return err
+	}
+	decoded, err := hex.DecodeString(str)
+	if err != nil {
+		return err
+	}
+	copy(h[:], decoded)
+	return nil
 }
