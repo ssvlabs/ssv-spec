1e8fe8b6dafde602d14feeef78422652ac32916c
Gal Rogozinski <galrogogit@gmail.com>
2023-08-10 16:50:15 +0300

    undo formatting

--- qbft/spectest/tests/messages/create_round_change.go
+++ qbft/spectest/tests/messages/create_round_change.go
@@ -1,8 +1,6 @@
 package messages
 
-import (
-	"github.com/bloxapp/ssv-spec/qbft/spectest/tests"
-)
+import "github.com/bloxapp/ssv-spec/qbft/spectest/tests"
 
 // CreateRoundChange tests creating a round change msg, not previously prepared
 func CreateRoundChange() tests.SpecTest {
