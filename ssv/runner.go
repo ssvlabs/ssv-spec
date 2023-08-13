a907617eabf6b5adcedd9672afde0ffc56cb85ca
Gal Rogozinski <galrogogit@gmail.com>
2023-08-13 12:50:03 +0300

    fix runner.go

--- ssv/runner.go
+++ ssv/runner.go
@@ -106,11 +106,8 @@ func (b *BaseRunner) baseConsensusMsgProcessing(runner Runner, msg *qbft.SignedM
 		prevDecided, _ = b.State.RunningInstance.IsDecided()
 	}
 
-	// TODO: revert `if false` after pre-consensus justification is fixed.
-	if false {
-		if err := b.processPreConsensusJustification(runner, b.highestDecidedSlot, msg); err != nil {
-			return false, nil, errors.Wrap(err, "invalid pre-consensus justification")
-		}
+	if err := b.processPreConsensusJustification(runner, b.highestDecidedSlot, msg); err != nil {
+		return false, nil, errors.Wrap(err, "invalid pre-consensus justification")
 	}
 
 	decidedMsg, err := b.QBFTController.ProcessMsg(msg)
