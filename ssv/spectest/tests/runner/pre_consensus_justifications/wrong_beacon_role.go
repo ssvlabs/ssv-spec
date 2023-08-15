d0f5c3b2c8a95df24151720ae5c3a5352d5889e7
Gal Rogozinski <galrogogit@gmail.com>
2023-08-15 12:06:26 +0300

    fix wrong beacon role

--- ssv/spectest/tests/runner/pre_consensus_justifications/wrong_beacon_role.go
+++ ssv/spectest/tests/runner/pre_consensus_justifications/wrong_beacon_role.go
@@ -19,7 +19,7 @@ func WrongBeaconRole() tests.SpecTest {
 		root, _ := qbft.HashDataRoot(fullData)
 		msg := &qbft.Message{
 			MsgType:    qbft.ProposalMsgType,
-			Height:     1,
+			Height:     testingutils.TestingDutySlot,
 			Round:      qbft.FirstRound,
 			Identifier: id,
 			Root:       root,
