a42beb6a59641e7a1bad04f4e71d1144d1a3f828
Gal Rogozinski <galrogogit@gmail.com>
2023-09-05 13:56:53 +0300

    Revert "tests now pass"
    This reverts commit 8705dd21c62581db709c3085342f06d95194bf5a.

--- types/testingutils/ssv_msgs.go
+++ types/testingutils/ssv_msgs.go
@@ -698,7 +698,7 @@ var PreConsensusContributionProofMsg = func(msgSK, beaconSK *bls.SecretKey, msgI
 }
 
 var PreConsensusContributionProofWrongBeaconSigMsg = func(msgSK, beaconSK *bls.SecretKey, msgID, beaconID types.OperatorID) *types.SignedPartialSignatureMessage {
-	return contributionProofMsg(msgSK, beaconSK, msgID, beaconID, TestingDutySlot, TestingDutySlot, false, true)
+	return contributionProofMsg(msgSK, beaconSK, msgID, beaconID, TestingDutySlot, TestingDutySlot+1, false, true)
 }
 
 var PreConsensusContributionProofNextEpochMsg = func(msgSK, beaconSK *bls.SecretKey, msgID, beaconID types.OperatorID) *types.SignedPartialSignatureMessage {
