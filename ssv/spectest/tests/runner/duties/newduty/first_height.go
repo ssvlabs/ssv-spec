15e21a26c3485ad559d8567f88d50c7f7eddbee1
Gal Rogozinski <galrogogit@gmail.com>
2023-10-23 17:34:04 +0300

    fix broadcast messages

--- ssv/spectest/tests/runner/duties/newduty/first_height.go
+++ ssv/spectest/tests/runner/duties/newduty/first_height.go
@@ -1,8 +1,6 @@
 package newduty
 
 import (
-	"github.com/attestantio/go-eth2-client/spec"
-
 	"github.com/bloxapp/ssv-spec/ssv/spectest/tests"
 	"github.com/bloxapp/ssv-spec/types"
 	"github.com/bloxapp/ssv-spec/types/testingutils"
@@ -15,45 +13,43 @@ func FirstHeight() tests.SpecTest {
 		Name: "new duty first height",
 		Tests: []*StartNewRunnerDutySpecTest{
 			{
-				Name:                    "sync committee aggregator",
-				Runner:                  testingutils.SyncCommitteeContributionRunner(ks),
-				Duty:                    &testingutils.TestingSyncCommitteeContributionDutyFirstSlot,
-				PostDutyRunnerStateRoot: "29862cc6054edc8547efcb5ae753290971d664b9c39768503b4d66e1b52ecb06",
+				Name:   "sync committee aggregator",
+				Runner: testingutils.SyncCommitteeContributionRunner(ks),
+				Duty:   &testingutils.TestingSyncCommitteeContributionDutyFirstSlot,
 				OutputMessages: []*types.SignedPartialSignatureMessage{
-					testingutils.PreConsensusContributionProofMsg(ks.Shares[1], ks.Shares[1], 1, 1), // broadcasts when starting a new duty
+					testingutils.PreConsensusCustomSlotContributionProofMsg(ks.Shares[1], ks.Shares[1], 1, 1, 0),
+					// broadcasts when starting a new duty
 				},
 			},
 			{
-				Name:                    "sync committee",
-				Runner:                  testingutils.SyncCommitteeRunner(ks),
-				Duty:                    &testingutils.TestingSyncCommitteeDutyFirstSlot,
-				PostDutyRunnerStateRoot: "f1587ce0947c4cb592d8a6b95453aede2a0da6a1f4e185f45cd6e5e304da0f82",
-				OutputMessages:          []*types.SignedPartialSignatureMessage{},
+				Name:           "sync committee",
+				Runner:         testingutils.SyncCommitteeRunner(ks),
+				Duty:           &testingutils.TestingSyncCommitteeDutyFirstSlot,
+				OutputMessages: []*types.SignedPartialSignatureMessage{},
 			},
 			{
-				Name:                    "aggregator",
-				Runner:                  testingutils.AggregatorRunner(ks),
-				Duty:                    &testingutils.TestingAggregatorDutyFirstSlot,
-				PostDutyRunnerStateRoot: "c54e71de23c3957b73abbb0e7b9e195b3f8f6370d62fbec256224faecf177fee",
+				Name:   "aggregator",
+				Runner: testingutils.AggregatorRunner(ks),
+				Duty:   &testingutils.TestingAggregatorDutyFirstSlot,
 				OutputMessages: []*types.SignedPartialSignatureMessage{
-					testingutils.PreConsensusSelectionProofMsg(ks.Shares[1], ks.Shares[1], 1, 1), // broadcasts when starting a new duty
+					testingutils.PreConsensusCustomSlotSelectionProofMsg(ks.Shares[1], ks.Shares[1], 1, 1, 0),
+					// broadcasts when starting a new duty
 				},
 			},
 			{
-				Name:                    "proposer",
-				Runner:                  testingutils.ProposerRunner(ks),
-				Duty:                    &testingutils.TestingProposerDutyFirstSlot,
-				PostDutyRunnerStateRoot: "56eafcb33392ded888a0fefe30ba49e52aa00ab36841cb10c9dc1aa2935af347",
+				Name:   "proposer",
+				Runner: testingutils.ProposerRunner(ks),
+				Duty:   &testingutils.TestingProposerDutyFirstSlot,
 				OutputMessages: []*types.SignedPartialSignatureMessage{
-					testingutils.PreConsensusRandaoMsgV(ks.Shares[1], 1, spec.DataVersionBellatrix), // broadcasts when starting a new duty
+					testingutils.PreConsensusRandaoMsg(ks.Shares[1], 1),
+					// broadcasts when starting a new duty
 				},
 			},
 			{
-				Name:                    "attester",
-				Runner:                  testingutils.AttesterRunner(ks),
-				Duty:                    &testingutils.TestingAttesterDutyFirstSlot,
-				PostDutyRunnerStateRoot: "a52249c78fe1b1b0ee793328c1fe2a53e70c7684fdf51b64da235163a4682fdd",
-				OutputMessages:          []*types.SignedPartialSignatureMessage{},
+				Name:           "attester",
+				Runner:         testingutils.AttesterRunner(ks),
+				Duty:           &testingutils.TestingAttesterDutyFirstSlot,
+				OutputMessages: []*types.SignedPartialSignatureMessage{},
 			},
 		},
 	}
