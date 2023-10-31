2b013b40ca5c34ea5d10863f4e52f606523724c0
Gal Rogozinski <galrogogit@gmail.com>
2023-10-31 22:07:11 +0200

    old duty slot

--- ssv/spectest/tests/runner/duties/newduty/old_slot_duty.go
+++ ssv/spectest/tests/runner/duties/newduty/old_slot_duty.go
@@ -14,20 +14,20 @@ import (
 func OldSlotDuty() tests.SpecTest {
 	ks := testingutils.Testing4SharesSet()
 
-	futureDecide := func(r ssv.Runner, duty *types.Duty) ssv.Runner {
+	futureDecide := func(r ssv.Runner, duty types.Duty) ssv.Runner {
 		duty.Slot += 50
-		r.GetBaseRunner().State = ssv.NewRunnerState(3, duty)
+		r.GetBaseRunner().State = ssv.NewRunnerState(3, &duty)
 		return r
 	}
 
 	expectedError := "can't start duty: duty for slot 12 already passed. Current height is 62"
 
 	return &MultiStartNewRunnerDutySpecTest{
-		Name: "new duty post future decided",
+		Name: "new duty old slot",
 		Tests: []*StartNewRunnerDutySpecTest{
 			{
 				Name:                    "sync committee aggregator",
-				Runner:                  futureDecide(testingutils.SyncCommitteeContributionRunner(ks), &testingutils.TestingSyncCommitteeContributionDuty),
+				Runner:                  futureDecide(testingutils.SyncCommitteeContributionRunner(ks), testingutils.TestingSyncCommitteeContributionDuty),
 				Duty:                    &testingutils.TestingSyncCommitteeContributionDuty,
 				PostDutyRunnerStateRoot: "0c2bf5b2570aad7da85cfe5aa81361524a727265c22b41e72e7e9ff2f9b2c215",
 				OutputMessages: []*types.SignedPartialSignatureMessage{
@@ -37,7 +37,7 @@ func OldSlotDuty() tests.SpecTest {
 			},
 			{
 				Name:                    "sync committee",
-				Runner:                  futureDecide(testingutils.SyncCommitteeRunner(ks), &testingutils.TestingSyncCommitteeDuty),
+				Runner:                  futureDecide(testingutils.SyncCommitteeRunner(ks), testingutils.TestingSyncCommitteeDuty),
 				Duty:                    &testingutils.TestingSyncCommitteeDuty,
 				PostDutyRunnerStateRoot: "89edc9d9c28654a0113c3003c1538aaae36fe14490992dbf057b9f5e5d492e33",
 				OutputMessages:          []*types.SignedPartialSignatureMessage{},
@@ -45,7 +45,7 @@ func OldSlotDuty() tests.SpecTest {
 			},
 			{
 				Name:                    "aggregator",
-				Runner:                  futureDecide(testingutils.AggregatorRunner(ks), &testingutils.TestingAggregatorDuty),
+				Runner:                  futureDecide(testingutils.AggregatorRunner(ks), testingutils.TestingAggregatorDuty),
 				Duty:                    &testingutils.TestingAggregatorDuty,
 				PostDutyRunnerStateRoot: "892d0c2842a81d163c59e53302085f7bb2753cc4fcb46593bfeb569ec39e8928",
 				OutputMessages: []*types.SignedPartialSignatureMessage{
@@ -55,7 +55,7 @@ func OldSlotDuty() tests.SpecTest {
 			},
 			{
 				Name:                    "proposer",
-				Runner:                  futureDecide(testingutils.ProposerRunner(ks), testingutils.TestingProposerDutyV(spec.DataVersionBellatrix)),
+				Runner:                  futureDecide(testingutils.ProposerRunner(ks), *testingutils.TestingProposerDutyV(spec.DataVersionBellatrix)),
 				Duty:                    testingutils.TestingProposerDutyV(spec.DataVersionBellatrix),
 				PostDutyRunnerStateRoot: "fdaaa35d42c3001cd891209a44b921fa64320be238794e01633661e16c4f5e02",
 				OutputMessages: []*types.SignedPartialSignatureMessage{
@@ -65,7 +65,7 @@ func OldSlotDuty() tests.SpecTest {
 			},
 			{
 				Name:                    "attester",
-				Runner:                  futureDecide(testingutils.AttesterRunner(ks), &testingutils.TestingAttesterDuty),
+				Runner:                  futureDecide(testingutils.AttesterRunner(ks), testingutils.TestingAttesterDuty),
 				Duty:                    &testingutils.TestingAttesterDuty,
 				PostDutyRunnerStateRoot: "ca53abb401eaae1154b075d5fc6ddca2da760c097fc30da8ee8e3abb94efb6d2",
 				OutputMessages:          []*types.SignedPartialSignatureMessage{},
