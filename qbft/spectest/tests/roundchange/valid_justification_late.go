2dc60144d4388c8e7b8d1758bacfb763127df567
Gal Rogozinski <galrogogit@gmail.com>
2023-11-19 15:50:15 +0200

    late justification

---
+++ qbft/spectest/tests/roundchange/valid_justification_late.go
@@ -0,0 +1,45 @@
+package roundchange
+
+import (
+	"github.com/bloxapp/ssv-spec/qbft"
+	"github.com/bloxapp/ssv-spec/qbft/spectest/tests"
+	"github.com/bloxapp/ssv-spec/types"
+	"github.com/bloxapp/ssv-spec/types/testingutils"
+)
+
+// ValidJustificationLate tests a valid rc quorum justification
+func ValidJustificationLate() tests.SpecTest {
+	ks := testingutils.Testing4SharesSet()
+	pre := testingutils.BaseInstance()
+	pre.State.ProposalAcceptedForCurrentRound = nil // proposal resets on upon timeout
+	pre.State.Round = 2
+
+	prepareMsgs := []*qbft.SignedMessage{
+		testingutils.TestingPrepareMessage(ks.Shares[1], types.OperatorID(1)),
+		testingutils.TestingPrepareMessage(ks.Shares[2], types.OperatorID(2)),
+		testingutils.TestingPrepareMessage(ks.Shares[3], types.OperatorID(3)),
+	}
+	msgs := []*qbft.SignedMessage{
+		testingutils.TestingRoundChangeMessageWithRound(ks.Shares[1], types.OperatorID(1), 2),
+		testingutils.TestingRoundChangeMessageWithRoundAndRC(ks.Shares[2], types.OperatorID(2), 2,
+			testingutils.MarshalJustifications(prepareMsgs)),
+		testingutils.TestingRoundChangeMessageWithRoundAndRC(ks.Shares[3], types.OperatorID(3), 2,
+			testingutils.MarshalJustifications(prepareMsgs)),
+		testingutils.TestingRoundChangeMessageWithRoundAndRC(ks.Shares[4], types.OperatorID(4), 2,
+			testingutils.MarshalJustifications(prepareMsgs)),
+	}
+
+	return &tests.MsgProcessingSpecTest{
+		Name:          "valid justification",
+		Pre:           pre,
+		InputMessages: msgs,
+		OutputMessages: []*qbft.SignedMessage{
+			testingutils.TestingProposalMessageWithParams(ks.Shares[1], types.OperatorID(1), 2, qbft.FirstHeight,
+				testingutils.TestingQBFTRootData, testingutils.MarshalJustifications(msgs[:len(msgs)-1]),
+				testingutils.MarshalJustifications(prepareMsgs)),
+			testingutils.TestingProposalMessageWithParams(ks.Shares[1], types.OperatorID(1), 2, qbft.FirstHeight,
+				testingutils.TestingQBFTRootData,
+				testingutils.MarshalJustifications(msgs), testingutils.MarshalJustifications(prepareMsgs)),
+		},
+	}
+}
