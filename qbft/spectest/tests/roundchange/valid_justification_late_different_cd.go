b82057337c70e09da2808ee3b7e592a677e55a36
Gal Rogozinski <galrogogit@gmail.com>
2023-11-20 17:02:29 +0200

    valid_justification_late_different_cd.go

---
+++ qbft/spectest/tests/roundchange/valid_justification_late_different_cd.go
@@ -0,0 +1,46 @@
+package roundchange
+
+import (
+	"crypto/sha256"
+	"github.com/bloxapp/ssv-spec/qbft"
+	"github.com/bloxapp/ssv-spec/qbft/spectest/tests"
+	"github.com/bloxapp/ssv-spec/types"
+	"github.com/bloxapp/ssv-spec/types/testingutils"
+)
+
+// ValidJustificationLateCD tests a valid rc quorum justification that comes in late that has different consensus data
+func ValidJustificationLateDifferentCD() tests.SpecTest {
+	ks := testingutils.Testing4SharesSet()
+	pre := testingutils.BaseInstance()
+	pre.State.ProposalAcceptedForCurrentRound = nil // proposal resets on upon timeout
+	pre.State.Round = 2
+	testData := []byte{1, 2}
+	testDataRoot := sha256.Sum256(testData)
+
+	prepareMsgs := []*qbft.SignedMessage{
+		testingutils.TestingPrepareMessageWithFullData(ks.Shares[1], types.OperatorID(1), testData),
+		testingutils.TestingPrepareMessageWithFullData(ks.Shares[2], types.OperatorID(2), testData),
+		testingutils.TestingPrepareMessageWithFullData(ks.Shares[3], types.OperatorID(3), testData),
+	}
+	msgs := []*qbft.SignedMessage{
+		testingutils.TestingRoundChangeMessageWithRound(ks.Shares[1], types.OperatorID(1), 2),
+		testingutils.TestingRoundChangeMessageWithRound(ks.Shares[2], types.OperatorID(2), 2),
+		testingutils.TestingRoundChangeMessageWithRound(ks.Shares[3], types.OperatorID(3), 2),
+		testingutils.TestingRoundChangeMessageWithParamsAndFullData(ks.Shares[4], types.OperatorID(4), 2, qbft.FirstHeight,
+			testDataRoot, 1, testData, testingutils.MarshalJustifications(prepareMsgs)),
+	}
+
+	return &tests.MsgProcessingSpecTest{
+		Name:          "valid justification late different cd",
+		Pre:           pre,
+		InputMessages: msgs,
+		OutputMessages: []*qbft.SignedMessage{
+			testingutils.TestingProposalMessageWithParams(ks.Shares[1], types.OperatorID(1), 2, qbft.FirstHeight,
+				testingutils.TestingQBFTRootData, testingutils.MarshalJustifications(msgs[:len(msgs)-1]),
+				[][]byte{}),
+			testingutils.TestingProposalMessageWithParams(ks.Shares[1], types.OperatorID(1), 2, qbft.FirstHeight,
+				testDataRoot,
+				testingutils.MarshalJustifications(msgs), testingutils.MarshalJustifications(prepareMsgs)),
+		},
+	}
+}
