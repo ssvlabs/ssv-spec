031ae8452390b44e3d7fdb32d83cf63d12214f6e
Gal Rogozinski <galrogogit@gmail.com>
2023-11-20 17:05:29 +0200

    late

--- qbft/spectest/tests/roundchange/valid_justification_late.go
+++ qbft/spectest/tests/roundchange/valid_justification_late.go
@@ -1,7 +1,6 @@
 package roundchange
 
 import (
-	"crypto/sha256"
 	"github.com/bloxapp/ssv-spec/qbft"
 	"github.com/bloxapp/ssv-spec/qbft/spectest/tests"
 	"github.com/bloxapp/ssv-spec/types"
@@ -14,32 +13,32 @@ func ValidJustificationLate() tests.SpecTest {
 	pre := testingutils.BaseInstance()
 	pre.State.ProposalAcceptedForCurrentRound = nil // proposal resets on upon timeout
 	pre.State.Round = 2
-	testData := []byte{1, 2}
-	testDataRoot := sha256.Sum256(testData)
 
 	prepareMsgs := []*qbft.SignedMessage{
-		testingutils.TestingPrepareMessageWithFullData(ks.Shares[1], types.OperatorID(1), testData),
-		testingutils.TestingPrepareMessageWithFullData(ks.Shares[2], types.OperatorID(2), testData),
-		testingutils.TestingPrepareMessageWithFullData(ks.Shares[3], types.OperatorID(3), testData),
+		testingutils.TestingPrepareMessage(ks.Shares[1], types.OperatorID(1)),
+		testingutils.TestingPrepareMessage(ks.Shares[2], types.OperatorID(2)),
+		testingutils.TestingPrepareMessage(ks.Shares[3], types.OperatorID(3)),
 	}
 	msgs := []*qbft.SignedMessage{
 		testingutils.TestingRoundChangeMessageWithRound(ks.Shares[1], types.OperatorID(1), 2),
-		testingutils.TestingRoundChangeMessageWithRound(ks.Shares[2], types.OperatorID(2), 2),
-		testingutils.TestingRoundChangeMessageWithRound(ks.Shares[3], types.OperatorID(3), 2),
-		testingutils.TestingRoundChangeMessageWithParamsAndFullData(ks.Shares[4], types.OperatorID(4), 2, qbft.FirstHeight,
-			testDataRoot, 1, testData, testingutils.MarshalJustifications(prepareMsgs)),
+		testingutils.TestingRoundChangeMessageWithRoundAndRC(ks.Shares[2], types.OperatorID(2), 2,
+			testingutils.MarshalJustifications(prepareMsgs)),
+		testingutils.TestingRoundChangeMessageWithRoundAndRC(ks.Shares[3], types.OperatorID(3), 2,
+			testingutils.MarshalJustifications(prepareMsgs)),
+		testingutils.TestingRoundChangeMessageWithRoundAndRC(ks.Shares[4], types.OperatorID(4), 2,
+			testingutils.MarshalJustifications(prepareMsgs)),
 	}
 
 	return &tests.MsgProcessingSpecTest{
-		Name:          "valid justification",
+		Name:          "valid justification late",
 		Pre:           pre,
 		InputMessages: msgs,
 		OutputMessages: []*qbft.SignedMessage{
 			testingutils.TestingProposalMessageWithParams(ks.Shares[1], types.OperatorID(1), 2, qbft.FirstHeight,
 				testingutils.TestingQBFTRootData, testingutils.MarshalJustifications(msgs[:len(msgs)-1]),
-				[][]byte{}),
+				testingutils.MarshalJustifications(prepareMsgs)),
 			testingutils.TestingProposalMessageWithParams(ks.Shares[1], types.OperatorID(1), 2, qbft.FirstHeight,
-				testDataRoot,
+				testingutils.TestingQBFTRootData,
 				testingutils.MarshalJustifications(msgs), testingutils.MarshalJustifications(prepareMsgs)),
 		},
 	}
