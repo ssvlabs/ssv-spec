ba2ad69d9a7091347ab604e469bbb6668690634e
MatheusFranco99 <48058141+MatheusFranco99@users.noreply.github.com>
2024-01-03 05:46:55 -0300

    Invalid data [skip ci]

---
+++ ssv/spectest/tests/validator/invalid_data_consensus.go
@@ -0,0 +1,29 @@
+package validator
+
+import (
+	"github.com/bloxapp/ssv-spec/ssv/spectest/tests"
+	"github.com/bloxapp/ssv-spec/types"
+	"github.com/bloxapp/ssv-spec/types/testingutils"
+)
+
+func InvalidDataConsensus() tests.SpecTest {
+	ks := testingutils.Testing4SharesSet()
+
+	msgs := []*types.SSVMessage{
+		{
+			MsgType: types.SSVConsensusMsgType,
+			MsgID:   types.NewMsgID(testingutils.TestingSSVDomainType, testingutils.TestingValidatorPubKey[:], types.BNRoleAttester),
+			Data:    []byte{1, 2, 3, 4},
+		},
+	}
+
+	return &ValidatorTest{
+		Name: "invalid data consensus",
+
+		KeySet:                 ks,
+		Messages:               msgs,
+		OutputMessages:         []*types.SSVMessage{},
+		BeaconBroadcastedRoots: []string{},
+		ExpectedError:          "could not get consensus Message from network Message: incorrect size",
+	}
+}
---
+++ ssv/spectest/tests/validator/invalid_data_partial_sig.go
@@ -0,0 +1,29 @@
+package validator
+
+import (
+	"github.com/bloxapp/ssv-spec/ssv/spectest/tests"
+	"github.com/bloxapp/ssv-spec/types"
+	"github.com/bloxapp/ssv-spec/types/testingutils"
+)
+
+func InvalidDataPartialSig() tests.SpecTest {
+	ks := testingutils.Testing4SharesSet()
+
+	msgs := []*types.SSVMessage{
+		{
+			MsgType: types.SSVPartialSignatureMsgType,
+			MsgID:   types.NewMsgID(testingutils.TestingSSVDomainType, testingutils.TestingValidatorPubKey[:], types.BNRoleAttester),
+			Data:    []byte{1, 2, 3, 4},
+		},
+	}
+
+	return &ValidatorTest{
+		Name: "invalid data partial sig",
+
+		KeySet:                 ks,
+		Messages:               msgs,
+		OutputMessages:         []*types.SSVMessage{},
+		BeaconBroadcastedRoots: []string{},
+		ExpectedError:          "could not get post consensus Message from network Message: incorrect size",
+	}
+}
