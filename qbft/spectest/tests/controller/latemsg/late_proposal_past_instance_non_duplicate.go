39a3ac641664351da4af5aa32cef8e3ad1763003
MatheusFranco99 <48058141+MatheusFranco99@users.noreply.github.com>
2024-01-11 08:52:38 -0300

    Drop instance data for height 1

--- qbft/spectest/tests/controller/latemsg/late_proposal_past_instance_non_duplicate.go
+++ qbft/spectest/tests/controller/latemsg/late_proposal_past_instance_non_duplicate.go
@@ -13,41 +13,35 @@ import (
 func LateProposalPastInstanceNonDuplicate() tests.SpecTest {
 	ks := testingutils.Testing4SharesSet()
 
-	allMsgs := testingutils.DecidingMsgsForHeightWithRoot(testingutils.TestingQBFTRootData,
-		testingutils.TestingQBFTFullData, testingutils.TestingIdentifier, 1, ks)
+	allMsgsForFirstHeight := testingutils.DecidingMsgsForHeightWithRoot(testingutils.TestingQBFTRootData,
+		testingutils.TestingQBFTFullData, testingutils.TestingIdentifier, qbft.FirstHeight, ks)
 
-	msgPerHeight := make(map[qbft.Height][]*qbft.SignedMessage)
-	msgPerHeight[qbft.FirstHeight] = allMsgs[0:7]
-	msgPerHeight[1] = allMsgs[7:14]
-
-	instanceData := func(height qbft.Height) *tests.RunInstanceData {
-		sc := lateProposalPastInstanceStateComparison(height, nil)
-		return &tests.RunInstanceData{
-			InputValue:    []byte{1, 2, 3, 4},
-			InputMessages: msgPerHeight[height],
-			ExpectedDecidedState: tests.DecidedState{
-				DecidedVal: testingutils.TestingQBFTFullData,
-				DecidedCnt: 1,
-				BroadcastedDecided: testingutils.TestingCommitMultiSignerMessageWithHeight(
-					[]*bls.SecretKey{ks.Shares[1], ks.Shares[2], ks.Shares[3]},
-					[]types.OperatorID{1, 2, 3},
-					height,
-				),
-			},
-			ControllerPostRoot:  sc.Root(),
-			ControllerPostState: sc.ExpectedState,
-		}
+	scFirstHeight := lateProposalPastInstanceStateComparison(qbft.FirstHeight, nil)
+	instanceData := &tests.RunInstanceData{
+		InputValue:    []byte{1, 2, 3, 4},
+		InputMessages: allMsgsForFirstHeight,
+		ExpectedDecidedState: tests.DecidedState{
+			DecidedVal: testingutils.TestingQBFTFullData,
+			DecidedCnt: 1,
+			BroadcastedDecided: testingutils.TestingCommitMultiSignerMessageWithHeight(
+				[]*bls.SecretKey{ks.Shares[1], ks.Shares[2], ks.Shares[3]},
+				[]types.OperatorID{1, 2, 3},
+				qbft.FirstHeight,
+			),
+		},
+		ControllerPostRoot:  scFirstHeight.Root(),
+		ControllerPostState: scFirstHeight.ExpectedState,
 	}
 
 	// Late proposal message for first height and round 2
 	lateMsg := testingutils.TestingMultiSignerProposalMessageWithParams([]*bls.SecretKey{ks.Shares[1]}, []types.OperatorID{1}, 2, qbft.FirstHeight, testingutils.TestingIdentifier, testingutils.TestingQBFTFullData, testingutils.TestingQBFTRootData)
-	sc := lateProposalPastInstanceStateComparison(2, lateMsg)
+
+	sc := lateProposalPastInstanceStateComparison(1, lateMsg)
 
 	return &tests.ControllerSpecTest{
 		Name: "late non duplicate proposal past instance",
 		RunInstanceData: []*tests.RunInstanceData{
-			instanceData(qbft.FirstHeight),
-			instanceData(1),
+			instanceData,
 			{
 				InputValue: []byte{1, 2, 3, 4},
 				InputMessages: []*qbft.SignedMessage{
