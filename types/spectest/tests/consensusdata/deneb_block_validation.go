97504bb515b4c36ce039c05600a8ed84e828a1d0
Gal Rogozinski <galrogogit@gmail.com>
2024-01-16 03:09:43 +0200

    add deneb tests

--- types/spectest/tests/consensusdata/deneb_block_validation.go
+++ types/spectest/tests/consensusdata/deneb_block_validation.go
@@ -1,6 +1,16 @@
 package consensusdata
 
+import (
+	"github.com/attestantio/go-eth2-client/spec"
+	"github.com/bloxapp/ssv-spec/types/testingutils"
+)
+
 // DenebBlockValidation tests a valid consensus data with deneb block
-func DenebBlockValidation() *SpecTest {
-	panic("implement")
+func DenebBlockValidation() *ConsensusDataTest {
+	ks := testingutils.Testing4SharesSet()
+
+	return &ConsensusDataTest{
+		Name:          "valid deneb block",
+		ConsensusData: *testingutils.TestProposerWithJustificationsConsensusDataV(ks, spec.DataVersionDeneb),
+	}
 }
--- types/spectest/tests/consensusdata/invalid_deneb_block_validation.go
+++ types/spectest/tests/consensusdata/invalid_deneb_block_validation.go
@@ -1,6 +1,22 @@
 package consensusdata
 
+import (
+	"github.com/attestantio/go-eth2-client/spec"
+	"github.com/bloxapp/ssv-spec/types"
+	"github.com/bloxapp/ssv-spec/types/testingutils"
+)
+
 // InvalidDenebBlockValidation tests an invalid consensus data with deneb block
-func InvalidDenebBlockValidation() *SpecTest {
-	panic("implement")
+func InvalidDenebBlockValidation() *ConsensusDataTest {
+	version := spec.DataVersionDeneb
+	cd := &types.ConsensusData{
+		Duty:    *testingutils.TestingProposerDutyV(version),
+		Version: version,
+		DataSSZ: []byte{},
+	}
+	return &ConsensusDataTest{
+		Name:          "invalid deneb block",
+		ConsensusData: *cd,
+		ExpectedError: "could not unmarshal ssz: incorrect size",
+	}
 }
