1f37484108bfd64eb932126e78567d18bccec6aa
Gal Rogozinski <galrogogit@gmail.com>
2024-01-17 16:58:53 +0200

    dynamic timeout

--- qbft/timeout.go
+++ qbft/timeout.go
@@ -1,17 +1,51 @@
 package qbft
 
 import (
+	"github.com/attestantio/go-eth2-client/spec/phase0"
+	"github.com/bloxapp/ssv-spec/types"
 	"github.com/pkg/errors"
-	"time"
 )
 
 var (
-	quickTimeoutThreshold = Round(8)        //nolint
-	quickTimeout          = 2 * time.Second //nolint
-	slowTimeout           = 2 * time.Minute //nolint
+	quickTimeoutThreshold = Round(8) //nolint
+	// quickTimeout is the timeout in seconds for the first 8 rounds
+	quickTimeout uint64 = 2 // 2 seconds
+	// slowTimeout is the timeout in seconds for rounds after the first 8
+	slowTimeout uint64 = 120 // 2 minutes
 	// CutoffRound which round the instance should stop its timer and progress no further
 	CutoffRound = 15 // stop processing instances after 8*2+120*6 = 14.2 min (~ 2 epochs)
 )
+
+// RoundTimeout returns the unix epoch time (seconds) in which we should send a RC message
+// Called for all beacon duties other than proposals
+func RoundTimeout(round Round, height Height, baseDuration uint64, network types.BeaconNetwork) uint64 {
+	// Calculate additional timeout in seconds based on round
+	var additionalTimeout uint64
+	additionalTimeout = uint64(round) * quickTimeout
+	if round > quickTimeoutThreshold {
+		slowPortion := uint64(round-quickTimeoutThreshold) * slowTimeout
+		additionalTimeout += slowPortion
+	}
+
+	// Combine base duration and additional timeout
+	timeoutDuration := baseDuration + additionalTimeout
+
+	// Get the unix epoch start time of the duty seconds
+	dutyStartTime := uint64(network.EstimatedTimeAtSlot(phase0.Slot(height)))
+
+	// Calculate the time until the duty should start plus the timeout duration
+	return dutyStartTime + timeoutDuration
+}
+
+// AttestationOrSyncCommitteeTimeout returns the unix epoch time (seconds) in which we should send a RC message
+func AttestationOrSyncCommitteeTimeout(round Round, height Height, network types.BeaconNetwork) uint64 {
+	return RoundTimeout(round, height, 4, network)
+}
+
+// AggregationOrContributionTimeout returns the unix epoch time (seconds) in which we should send a RC message
+func AggregationOrContributionTimeout(r Round, height Height, network types.BeaconNetwork) uint64 {
+	return RoundTimeout(r, height, 8, network)
+}
 
 func (i *Instance) UponRoundTimeout() error {
 	if !i.CanProcessMessages() {
