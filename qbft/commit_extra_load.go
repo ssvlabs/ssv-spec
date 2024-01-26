3f67477d6eede4976496f9d4285b2ca1989cfdc1
MatheusFranco99 <48058141+MatheusFranco99@users.noreply.github.com>
2024-01-26 16:56:13 +0000

    Change CommitExtraLoad to hold signing root

--- qbft/commit_extra_load.go
+++ qbft/commit_extra_load.go
@@ -1,16 +1,33 @@
 package qbft
 
-import "github.com/bloxapp/ssv-spec/types"
+import (
+	"github.com/attestantio/go-eth2-client/spec/phase0"
+	"github.com/bloxapp/ssv-spec/types"
+)
+
+type PostConsensusSignature struct {
+	Signature   types.Signature `ssz-size:"96"`
+	SigningRoot phase0.Root     `ssz-size:"32"`
+}
+
+// Encode encoded bytes or error
+func (p *PostConsensusSignature) Encode() ([]byte, error) {
+	return p.MarshalSSZ()
+}
+
+// Decode returns error if decoding failed
+func (p *PostConsensusSignature) Decode(data []byte) error {
+	return p.UnmarshalSSZ(data)
+}
 
 // This structure is appended in commit messages carrying extra information.
 // It's useful for the underlying duty but not necessary for the consensus termination
 type CommitExtraLoad struct {
-	// List of signatures. We must use list instead of a unique element due to the sync committee aggregation duty.
-	Signatures []types.Signature `ssz-max:"4"` // Maximum number of sync committee subnets as defined in https://github.com/ethereum/consensus-specs/blob/dev/specs/altair/validator.md
+	PostConsensusSignatures []*PostConsensusSignature `ssz-max:"4"` // Maximum number of sync committee subnets as defined in https://github.com/ethereum/consensus-specs/blob/dev/specs/altair/validator.md
 }
 
-func (c *CommitExtraLoad) GetSignatures() []types.Signature {
-	return c.Signatures
+func (c *CommitExtraLoad) GetPostConsensusSignatures() []*PostConsensusSignature {
+	return c.PostConsensusSignatures
 }
 
 // Encode returns a CommitExtraLoad encoded bytes or error
--- qbft/generate.go
+++ qbft/generate.go
@@ -1,7 +1,9 @@
 package qbft
 
 //go:generate rm -f ./commit_extra_load_encoding.go
-//go:generate go run github.com/ferranbt/fastssz/sszgen --path commit_extra_load.go --include ./types.go,../types/signer.go
+//go:generate go run github.com/ferranbt/fastssz/sszgen --path commit_extra_load.go --include ./types.go,../types/signer.go,$GOPATH/pkg/mod/github.com/attestantio/go-eth2-client@v0.19.7/spec/phase0
+//go:generate go install golang.org/x/tools/cmd/goimports@latest
+//go:generate goimports -w commit_extra_load_encoding.go
 
 //go:generate rm -f ./messages_encoding.go
-//go:generate go run github.com/ferranbt/fastssz/sszgen --path messages.go --include ./types.go,../types/signer.go,../types/operator.go,./commit_extra_load.go --exclude-objs OperatorID
+//go:generate go run github.com/ferranbt/fastssz/sszgen --path messages.go --include ./types.go,../types/signer.go,../types/operator.go,./commit_extra_load.go,$GOPATH/pkg/mod/github.com/attestantio/go-eth2-client@v0.19.7/spec/phase0 --exclude-objs OperatorID
