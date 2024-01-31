3a91902701858b5a0f387fc573e1f10119958f1b
MatheusFranco99 <48058141+MatheusFranco99@users.noreply.github.com>
2024-01-31 10:45:55 +0000

    RC with diff round test

---
+++ qbft/spectest/tests/proposal/rc_with_lower_round.go
@@ -0,0 +1,43 @@
+package proposal
+
+import (
+	"github.com/bloxapp/ssv-spec/qbft"
+	"github.com/bloxapp/ssv-spec/qbft/spectest/tests"
+	"github.com/bloxapp/ssv-spec/types"
+	"github.com/bloxapp/ssv-spec/types/testingutils"
+)
+
+// RCWithDifferentRound tests a proposal for > 1 round with one RC message with different round
+func RCWithDifferentRound() tests.SpecTest {
+	pre := testingutils.BaseInstance()
+	ks := testingutils.Testing4SharesSet()
+
+	prepareMsgs := []*qbft.SignedMessage{
+		testingutils.TestingPrepareMessage(ks.Shares[1], types.OperatorID(1)),
+		testingutils.TestingPrepareMessage(ks.Shares[2], types.OperatorID(2)),
+		testingutils.TestingPrepareMessage(ks.Shares[3], types.OperatorID(3)),
+	}
+
+	rcMsgs := []*qbft.SignedMessage{
+		testingutils.TestingRoundChangeMessageWithRoundAndRC(ks.Shares[1], types.OperatorID(1), 1, // diff round
+			testingutils.MarshalJustifications(prepareMsgs)),
+		testingutils.TestingRoundChangeMessageWithRoundAndRC(ks.Shares[2], types.OperatorID(2), 2,
+			testingutils.MarshalJustifications(prepareMsgs)),
+		testingutils.TestingRoundChangeMessageWithRoundAndRC(ks.Shares[3], types.OperatorID(3), 2,
+			testingutils.MarshalJustifications(prepareMsgs)),
+	}
+
+	msgs := []*qbft.SignedMessage{
+		testingutils.TestingProposalMessageWithParams(ks.Shares[1], types.OperatorID(1), 2, qbft.FirstHeight,
+			testingutils.TestingQBFTRootData,
+			testingutils.MarshalJustifications(rcMsgs), testingutils.MarshalJustifications(prepareMsgs),
+		),
+	}
+	return &tests.MsgProcessingSpecTest{
+		Name:           "rc with different round",
+		Pre:            pre,
+		InputMessages:  msgs,
+		OutputMessages: []*qbft.SignedMessage{},
+		ExpectedError:  "invalid signed message: proposal not justified: change round msg not valid: wrong msg round",
+	}
+}
