32e51e3b1161cd5beb0a76431c48b88b72f84774
Gal Rogozinski <galrogogit@gmail.com>
2024-03-25 01:05:38 +0200

    fix tests

--- qbft/spectest/tests/startinstance/invalid_value.go
+++ qbft/spectest/tests/startinstance/invalid_value.go
@@ -6,6 +6,7 @@ import (
 )
 
 // InvalidValue tests a starting an instance for an invalid value (not passing value check)
+// Instance starts but with an empty value and no proposal gets broadcasted
 func InvalidValue() tests.SpecTest {
 	return &tests.ControllerSpecTest{
 		Name: "start instance invalid value",
@@ -15,6 +16,5 @@ func InvalidValue() tests.SpecTest {
 				ControllerPostRoot: "baf3ccea443a6c639b76dccf2d9c4fb5e48318473797de9b55e4d8de48fccc6b",
 			},
 		},
-		ExpectedError: "value invalid: invalid value",
 	}
 }
--- qbft/spectest/tests/startinstance/nil_value.go
+++ qbft/spectest/tests/startinstance/nil_value.go
@@ -5,6 +5,7 @@ import (
 )
 
 // NilValue tests a starting an instance for a nil value (not passing value check)
+// Instance starts but with an empty value and no proposal gets broadcasted
 func NilValue() tests.SpecTest {
 	return &tests.ControllerSpecTest{
 		Name: "start instance nil value",
@@ -14,6 +15,5 @@ func NilValue() tests.SpecTest {
 				ControllerPostRoot: "baf3ccea443a6c639b76dccf2d9c4fb5e48318473797de9b55e4d8de48fccc6b",
 			},
 		},
-		ExpectedError: "value invalid: invalid value",
 	}
 }
