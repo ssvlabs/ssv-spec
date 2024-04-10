e2ec610f8a4f14d70c327d7f1ad26ed8bc079b48
Gal Rogozinski <galrogogit@gmail.com>
2024-04-10 23:25:25 +0300

    add methods to operator and share

--- types/operator.go
+++ types/operator.go
@@ -1,14 +1,17 @@
 package types
 
+import "github.com/bloxapp/ssv-spec/ssv"
+
 // OperatorID is a unique ID for the node, used to create shares and verify msgs
 type OperatorID = uint64
 
 // Operator represents an SSV operator node that is part of a committee
 type Operator struct {
 	OperatorID        OperatorID
+	ClusterID         ssv.ClusterID
 	SSVOperatorPubKey []byte `ssz-size:"294"`
 	// TODO: change with one parameter F
-	Quorum, PartialQuorum uint32
+	Quorum, PartialQuorum uint64
 	Committee             []*CommitteeMember `ssz-max:"13"`
 }
 
@@ -16,4 +19,24 @@ type Operator struct {
 type CommitteeMember struct {
 	OperatorID        OperatorID
 	SSVOperatorPubKey []byte
+}
+
+// HasQuorum returns true if at least 2f+1 items are present (cnt is the number of items). It assumes nothing about those items, not their type or structure
+// https://github.com/ConsenSys/qbft-formal-spec-and-verification/blob/main/dafny/spec/L1/node_auxiliary_functions.dfy#L259
+func (operator *Operator) HasQuorum(cnt int) bool {
+	return uint64(cnt) >= operator.Quorum
+}
+
+// HasPartialQuorum returns true if at least f+1 items present (cnt is the number of items). It assumes nothing about those items, not their type or structure.
+// https://github.com/ConsenSys/qbft-formal-spec-and-verification/blob/main/dafny/spec/L1/node_auxiliary_functions.dfy#L244
+func (operator *Operator) HasPartialQuorum(cnt int) bool {
+	return uint64(cnt) >= operator.PartialQuorum
+}
+
+func (operator *Operator) Encode() ([]byte, error) {
+	return operator.MarshalSSZ()
+}
+
+func (operator *Operator) Decode(data []byte) error {
+	return operator.UnmarshalSSZ(data)
 }
--- types/share.go
+++ types/share.go
@@ -2,25 +2,19 @@ package types
 
 // Share holds all info about the validator share
 type Share struct {
-	ValidatorPubKey       ValidatorPK        `ssz-size:"48"`
-	SharePubKey           []ShareValidatorPK `ssz-size:"48"`
-	Committee             []ShareValidatorPK `ssz-max:"13"`
-	Quorum, PartialQuorum uint64
-	DomainType            DomainType `ssz-size:"4"`
-	FeeRecipientAddress   [20]byte   `ssz-size:"20"`
-	Graffiti              []byte     `ssz-size:"32"`
+	ValidatorPubKey     ValidatorPK        `ssz-size:"48"`
+	SharePubKey         []ShareValidatorPK `ssz-size:"48"`
+	Committee           []ShareValidatorPK `ssz-max:"13"`
+	Quorum              uint64
+	DomainType          DomainType `ssz-size:"4"`
+	FeeRecipientAddress [20]byte   `ssz-size:"20"`
+	Graffiti            []byte     `ssz-size:"32"`
 }
 
 // HasQuorum returns true if at least 2f+1 items are present (cnt is the number of items). It assumes nothing about those items, not their type or structure
 // https://github.com/ConsenSys/qbft-formal-spec-and-verification/blob/main/dafny/spec/L1/node_auxiliary_functions.dfy#L259
 func (share *Share) HasQuorum(cnt int) bool {
 	return uint64(cnt) >= share.Quorum
-}
-
-// HasPartialQuorum returns true if at least f+1 items present (cnt is the number of items). It assumes nothing about those items, not their type or structure.
-// https://github.com/ConsenSys/qbft-formal-spec-and-verification/blob/main/dafny/spec/L1/node_auxiliary_functions.dfy#L244
-func (share *Share) HasPartialQuorum(cnt int) bool {
-	return uint64(cnt) >= share.PartialQuorum
 }
 
 func (share *Share) Encode() ([]byte, error) {
