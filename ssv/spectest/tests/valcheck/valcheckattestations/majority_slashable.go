7c186ef4d7398fb1cff3e77339096e9d60644daf
Gal Rogozinski <galrogogit@gmail.com>
2024-05-12 23:26:54 +0300

    nolint

--- ssv/spectest/tests/valcheck/valcheckattestations/majority_slashable.go
+++ ssv/spectest/tests/valcheck/valcheckattestations/majority_slashable.go
@@ -2,6 +2,7 @@ package valcheckattestations
 
 import "github.com/ssvlabs/ssv-spec/ssv/spectest/tests"
 
+// nolint
 // majoritySlashable tests a slashable attestation by majority of validators
 func majoritySlashable() tests.SpecTest {
 	panic("implement me")
--- ssv/spectest/tests/valcheck/valcheckattestations/minority_slashable.go
+++ ssv/spectest/tests/valcheck/valcheckattestations/minority_slashable.go
@@ -2,6 +2,7 @@ package valcheckattestations
 
 import "github.com/ssvlabs/ssv-spec/ssv/spectest/tests"
 
+// nolint
 // minoritySlashable tests a slashable attestation by majority of validators
 func minoritySlashable() tests.SpecTest {
 	panic("implement me")
