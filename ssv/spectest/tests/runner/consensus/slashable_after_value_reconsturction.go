cbecd796d7743344e08f73b233e8bfef749b6c07
Gal Rogozinski <galrogogit@gmail.com>
2024-05-13 13:17:22 +0300

    delete unneeded test

--- ssv/spectest/tests/runner/consensus/slashable_after_value_reconsturction.go
+++
@@ -1,8 +0,0 @@
-package consensus
-
-import "github.com/ssvlabs/ssv-spec/ssv/spectest/tests"
-
-// SlashableAfterValueReconstruction tests a slashable value that passed consensus
-func SlashableAfterValueReconstruction() tests.SpecTest {
-	panic("implement me")
-}
