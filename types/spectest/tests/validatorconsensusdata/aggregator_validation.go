56a747923591a5612b05e1482c1bd1724698e0aa
Gal Rogozinski <galrogogit@gmail.com>
2024-07-01 17:06:42 +0300

    Delete preconsensus justifications (#436)
    * add DomainType to CommitteeMember
    
    * give domain value in tests and fix generate
    
    * generte files
    
    * delete preconsensus justifications
    
    * remove justifications from consensus_data.go
    
    * remove justifications from tests
    
    * one more fix
    
    * generate files
    
    * fix lint
    
    * fix message size
    
    * fix encoding
    
    * correct comment
    
    * generate files
    
    * fix size again
    
    * generate files

--- types/spectest/tests/validatorconsensusdata/aggregator_validation.go
+++ types/spectest/tests/validatorconsensusdata/aggregator_validation.go
@@ -4,10 +4,8 @@ import "github.com/ssvlabs/ssv-spec/types/testingutils"
 
 // AggregatorValidation tests a valid consensus data with AggregateAndProof
 func AggregatorValidation() *ValidatorConsensusDataTest {
-	ks := testingutils.Testing4SharesSet()
-
 	return &ValidatorConsensusDataTest{
 		Name:          "aggregator valid",
-		ConsensusData: *testingutils.TestAggregatorWithJustificationsConsensusData(ks),
+		ConsensusData: *testingutils.TestAggregatorConsensusData,
 	}
 }
