54d9b0f4f4513541287bfb310d088a0ef9a7560d
MatheusFranco99 <48058141+MatheusFranco99@users.noreply.github.com>
2024-08-13 15:09:48 +0100

    Remove nolint comment and export timeout variables

--- qbft/timeout.go
+++ qbft/timeout.go
@@ -1,14 +1,15 @@
 package qbft
 
 import (
-	"github.com/pkg/errors"
 	"time"
+
+	"github.com/pkg/errors"
 )
 
 var (
-	quickTimeoutThreshold = Round(8)        //nolint
-	quickTimeout          = 2 * time.Second //nolint
-	slowTimeout           = 2 * time.Minute //nolint
+	QuickTimeoutThreshold = Round(8)
+	QuickTimeout          = 2 * time.Second
+	SlowTimeout           = 2 * time.Minute
 	// CutoffRound which round the instance should stop its timer and progress no further
 	CutoffRound = 12 // stop processing attestations after 8*2+120*3 = 6.2 min (~ 1 epoch)
 )
