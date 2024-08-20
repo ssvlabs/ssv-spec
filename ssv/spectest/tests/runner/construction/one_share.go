f87147cb15b1c089492a19c4c29bd2ce5d07fadd
MatheusFranco99 <48058141+MatheusFranco99@users.noreply.github.com>
2024-08-20 10:25:29 +0100

    Spec - Share length validation (#478)
    * Add share length validation in runner construction
    
    * Align to error handling in runners constructions
    
    * Add validation to committee runner
    
    * Add runner construction tests
    
    * Refactor runner construction in testingutil to deal with creation errors
    
    * Generate JSON tests
    
    * Fix lint issue
    
    * Fix comments

---
+++ ssv/spectest/tests/runner/construction/one_share.go
@@ -0,0 +1,31 @@
+package runnerconstruction
+
+import (
+	"github.com/attestantio/go-eth2-client/spec/phase0"
+	"github.com/ssvlabs/ssv-spec/ssv/spectest/tests"
+	"github.com/ssvlabs/ssv-spec/types"
+	"github.com/ssvlabs/ssv-spec/types/testingutils"
+)
+
+func OneShare() tests.SpecTest {
+	ks := testingutils.Testing4SharesSet()
+	shares := testingutils.ShareMapFromKeySetMap(map[phase0.ValidatorIndex]*testingutils.TestKeySet{
+		testingutils.TestingValidatorIndex: ks,
+	})
+
+	// No errors since one share must be valid for all runners
+	expectedErrors := map[types.RunnerRole]string{
+		types.RoleCommittee:                 "",
+		types.RoleProposer:                  "",
+		types.RoleAggregator:                "",
+		types.RoleSyncCommitteeContribution: "",
+		types.RoleValidatorRegistration:     "",
+		types.RoleVoluntaryExit:             "",
+	}
+
+	return &RunnerConstructionSpecTest{
+		Name:      "one share",
+		Shares:    shares,
+		RoleError: expectedErrors,
+	}
+}
