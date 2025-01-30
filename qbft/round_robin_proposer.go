f2c559fd1915a4ceacaf13478d5122141f504010
MatheusFranco99 <48058141+MatheusFranco99@users.noreply.github.com>
2025-01-30 14:43:23 +0000

    refactor proposer function

--- qbft/round_robin_proposer.go
+++ qbft/round_robin_proposer.go
@@ -6,12 +6,13 @@ import "github.com/ssvlabs/ssv-spec/types"
 // Each new height starts with the first proposer and increments by 1 with each following round.
 // Each new height has a different first round proposer which is +1 from the previous height.
 // First height starts with index 0
-func RoundRobinProposer(state *State, round Round) types.OperatorID {
+// Assumption: the operators list is sorted.
+func RoundRobinProposer(height Height, round Round, operators []types.OperatorID) types.OperatorID {
 	firstRoundIndex := 0
-	if state.Height != FirstHeight {
-		firstRoundIndex += int(state.Height) % len(state.CommitteeMember.Committee)
+	if height != FirstHeight {
+		firstRoundIndex += int(height) % len(operators)
 	}
 
-	index := (firstRoundIndex + int(round) - int(FirstRound)) % len(state.CommitteeMember.Committee)
-	return state.CommitteeMember.Committee[index].OperatorID
+	index := (firstRoundIndex + int(round) - int(FirstRound)) % len(operators)
+	return operators[index]
 }
