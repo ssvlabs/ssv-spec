7f4337bafaa3479b18d313507edf0dd5e8b3680e
Alan <alan@ssvlabs.io>
2025-07-23 01:20:30 +0100

    export test key set instead of just private keys

--- types/testingutils/privatekeyinfo.go
+++ types/testingutils/privatekeyinfo.go
@@ -8,16 +8,32 @@ import (
 )
 
 type PrivateKeyInfo struct {
-	ValidatorSK  string
-	Shares       map[types.OperatorID]string
-	OperatorKeys map[types.OperatorID]string
+	ValidatorSK                             string
+	ValidatorPK                             string
+	ShareCount, Threshold, PartialThreshold uint64
+	Shares                                  map[types.OperatorID]string
+	OperatorKeys                            map[types.OperatorID]string
+	DKGOperators                            map[types.OperatorID]struct {
+		SK            string
+		ETHAddress    string
+		EncryptionKey string
+	}
 }
 
 func BuildPrivateKeyInfo(ks *TestKeySet) *PrivateKeyInfo {
 	privateKeyInfo := &PrivateKeyInfo{
-		ValidatorSK:  hex.EncodeToString(ks.ValidatorSK.Serialize()),
-		Shares:       make(map[types.OperatorID]string),
-		OperatorKeys: make(map[types.OperatorID]string),
+		ValidatorSK:      hex.EncodeToString(ks.ValidatorSK.Serialize()),
+		ValidatorPK:      hex.EncodeToString(ks.ValidatorPK.Serialize()),
+		ShareCount:       ks.ShareCount,
+		Threshold:        ks.Threshold,
+		PartialThreshold: ks.PartialThreshold,
+		Shares:           make(map[types.OperatorID]string),
+		OperatorKeys:     make(map[types.OperatorID]string),
+		DKGOperators: make(map[types.OperatorID]struct {
+			SK            string
+			ETHAddress    string
+			EncryptionKey string
+		}),
 	}
 
 	for operatorID, shareSK := range ks.Shares {
@@ -25,7 +41,19 @@ func BuildPrivateKeyInfo(ks *TestKeySet) *PrivateKeyInfo {
 	}
 
 	for operatorID, operatorKey := range ks.OperatorKeys {
-		privateKeyInfo.OperatorKeys[operatorID] = fmt.Sprintf("N:%s,E:%d", operatorKey.N.String(), operatorKey.E)
+		privateKeyInfo.OperatorKeys[operatorID] = fmt.Sprintf("N:%s,E:%d,D:%s", operatorKey.N.String(), operatorKey.E, operatorKey.D.String())
+	}
+
+	for operatorID, dkgOperator := range ks.DKGOperators {
+		privateKeyInfo.DKGOperators[operatorID] = struct {
+			SK            string
+			ETHAddress    string
+			EncryptionKey string
+		}{
+			SK:            dkgOperator.SK.D.String(),
+			ETHAddress:    dkgOperator.ETHAddress.Hex(),
+			EncryptionKey: fmt.Sprintf("N:%s,E:%d,D:%s", dkgOperator.EncryptionKey.N.String(), dkgOperator.EncryptionKey.E, dkgOperator.EncryptionKey.D.String()),
+		}
 	}
 
 	return privateKeyInfo
