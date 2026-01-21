d6a601d0ac0614c989925ece538e719d968fa882
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2026-01-21 19:49:43 +0300

    revert other consts

---
+++ types/spectest/tests/maxmsgsize/max_qbft_messages.go
@@ -0,0 +1,7 @@
+package maxmsgsize
+
+const (
+	MaxSizeQBFTMessageWithNoJustification = 132
+	MaxSizeQBFTMessageWith1Justification  = 48284
+	MaxSizeQBFTMessageWith2Justification  = 722412
+)
---
+++ types/spectest/tests/maxmsgsize/max_ssv_message.go
@@ -0,0 +1,6 @@
+package maxmsgsize
+
+const (
+	MaxSizeSSVMessageFromQBFTMessage              = 722480
+	MaxSizeSSVMessageFromPartialSignatureMessages = 727000
+)
