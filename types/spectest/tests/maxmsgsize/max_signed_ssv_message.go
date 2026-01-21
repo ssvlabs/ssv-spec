0d5fe625ed199aecd6e76be33c14b32ba8a234ed
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2026-01-21 19:46:59 +0300

    revert consts

--- types/spectest/tests/maxmsgsize/max_signed_ssv_message.go
+++ types/spectest/tests/maxmsgsize/max_signed_ssv_message.go
@@ -1,5 +1,9 @@
 package maxmsgsize
 
 const (
-	MaxSizeFullConsensusData = 8388836
+	MaxSizeSignedSSVMessageFromQBFTWithNoJustification  = 3700
+	MaxSizeSignedSSVMessageFromQBFTWith1Justification   = 51852
+	MaxSizeSignedSSVMessageFromQBFTWith2Justification   = 9114816
+	MaxSizeSignedSSVMessageFromPartialSignatureMessages = 730500
+	MaxSizeFullConsensusData                            = 8388836
 )
