7436c4c4aca04ddb91f5f7b013eb8b4caae098a6
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2026-01-21 18:33:04 +0300

    delete leftovers

--- types/spectest/tests/maxmsgsize/max_agg_comm_consensus_data.go
+++ types/spectest/tests/maxmsgsize/max_agg_comm_consensus_data.go
@@ -1,80 +1,16 @@
 package maxmsgsize
 
 import (
-	"github.com/attestantio/go-eth2-client/spec"
-	"github.com/attestantio/go-eth2-client/spec/altair"
 	"github.com/attestantio/go-eth2-client/spec/electra"
 	"github.com/attestantio/go-eth2-client/spec/phase0"
 	"github.com/prysmaticlabs/go-bitfield"
-	"github.com/ssvlabs/ssv-spec/types"
 	"github.com/ssvlabs/ssv-spec/types/spectest/testdoc"
 )
 
 const (
-	MaxSizeAggregatorCommitteeConsensusData = 8970524
-	MaxSizePhase0Attestation                = 2276
-	MaxSizeElectraAttestation               = 131308
+	MaxSizePhase0Attestation  = 2276
+	MaxSizeElectraAttestation = 131308
 )
-
-func maxAggregatorCommitteeConsensusData() *types.AggregatorCommitteeConsensusData {
-
-	maxVals := 3000
-
-	// Aggregator fields
-	aggs := make([]types.AssignedAggregator, 0)
-	for i := 0; i < maxVals; i++ {
-		aggs = append(aggs, types.AssignedAggregator{
-			ValidatorIndex: phase0.ValidatorIndex(i),
-			SelectionProof: phase0.BLSSignature([96]byte{1}),
-			CommitteeIndex: uint64(i),
-		})
-	}
-	maxCommIdxs := 64
-	aggCommIdxs := make([]uint64, 0)
-	for i := 0; i < maxCommIdxs; i++ {
-		aggCommIdxs = append(aggCommIdxs, 1)
-	}
-	aggAtt := make([][]byte, 0)
-	for i := 0; i < maxCommIdxs; i++ {
-		att := [MaxSizeElectraAttestation]byte{1}
-		aggAtt = append(aggAtt, att[:])
-	}
-
-	// Contributor fields
-	maxContribs := 2048
-	contributors := make([]types.AssignedAggregator, 0)
-	for i := 0; i < maxContribs; i++ {
-		contributors = append(contributors, types.AssignedAggregator{
-			ValidatorIndex: phase0.ValidatorIndex(i),
-			SelectionProof: phase0.BLSSignature([96]byte{1}),
-			CommitteeIndex: uint64(i),
-		})
-	}
-
-	maxSCC := 4
-	syncCommitteeContributions := make([]altair.SyncCommitteeContribution, 0)
-	bitvec := bitfield.NewBitvector128()
-	for i := 0; i < maxSCC; i++ {
-		syncCommitteeContributions = append(syncCommitteeContributions, altair.SyncCommitteeContribution{
-			Slot:              1,
-			BeaconBlockRoot:   [32]byte{1},
-			SubcommitteeIndex: 1,
-			AggregationBits:   bitvec,
-			Signature:         phase0.BLSSignature([96]byte{1}),
-		})
-	}
-
-	return &types.AggregatorCommitteeConsensusData{
-		Version: spec.DataVersionPhase0,
-
-		Aggregators:                 aggs,
-		AggregatorsCommitteeIndexes: aggCommIdxs,
-		AggregatedAttestations:      aggAtt,
-
-		Contributors:               contributors,
-		SyncCommitteeContributions: syncCommitteeContributions,
-	}
-}
 
 // Wrapper types to allow defining Encode/Decode methods on external types
 type Phase0AttestationWrapper struct {
