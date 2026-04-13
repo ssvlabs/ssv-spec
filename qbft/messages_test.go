6a98dbd512a1a78eba49b9df14e9b10286c9b848
oleg-ssvlabs <oleg.iar@ssvlabs.io>
2026-04-13 14:53:10 +0200

    Add more success/error path unit tests

--- qbft/messages_test.go
+++ qbft/messages_test.go
@@ -23,7 +23,26 @@ func TestRoundChangeJustificationProcessingMessagesReturnsDecodeError(t *testing
 
 	processingMessages, err := msg.RoundChangeJustificationProcessingMessages()
 	require.Nil(t, processingMessages)
-	require.Error(t, err)
+	require.ErrorContains(t, err, "decode justification message")
+}
+
+func TestRoundChangeJustificationProcessingMessages(t *testing.T) {
+	keySet := testingutils.Testing4SharesSet()
+	justification := testingutils.TestingRoundChangeMessageWithRound(keySet.OperatorKeys[1], types.OperatorID(1), 2)
+
+	justifications, err := qbft.MarshalJustifications([]*types.SignedSSVMessage{justification})
+	require.NoError(t, err)
+
+	msg := &qbft.Message{
+		RoundChangeJustification: justifications,
+	}
+
+	processingMessages, err := msg.RoundChangeJustificationProcessingMessages()
+	require.NoError(t, err)
+	require.Len(t, processingMessages, 1)
+	require.Equal(t, qbft.RoundChangeMsgType, processingMessages[0].QBFTMessage.MsgType)
+	require.Equal(t, qbft.FirstHeight, processingMessages[0].QBFTMessage.Height)
+	require.Equal(t, qbft.Round(2), processingMessages[0].QBFTMessage.Round)
 }
 
 func TestPrepareJustificationProcessingMessages(t *testing.T) {
@@ -44,3 +63,21 @@ func TestPrepareJustificationProcessingMessages(t *testing.T) {
 	require.Equal(t, qbft.FirstHeight, processingMessages[0].QBFTMessage.Height)
 	require.Equal(t, qbft.FirstRound, processingMessages[0].QBFTMessage.Round)
 }
+
+func TestPrepareJustificationProcessingMessagesReturnsDecodeError(t *testing.T) {
+	keySet := testingutils.Testing4SharesSet()
+	justification := testingutils.TestingPrepareMessage(keySet.OperatorKeys[1], types.OperatorID(1))
+	justification.SSVMessage.Data = []byte{1, 2, 3}
+
+	justifications, err := qbft.MarshalJustifications([]*types.SignedSSVMessage{justification})
+	require.NoError(t, err)
+
+	msg := &qbft.Message{
+		PrepareJustification: justifications,
+	}
+
+	processingMessages, err := msg.PrepareJustificationProcessingMessages()
+	require.Nil(t, processingMessages)
+	require.ErrorContains(t, err, "decode justification message")
+}
+
